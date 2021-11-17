77eabe04b11b5ed08cf537467b19b1a16e3cb511
Joon Hee Song <jhsong@vitasoft.co.kr>
2021-11-17 13:59:31 +0900

    백준: 4949번

---
+++ baekjoon/src/stack/No4949.java
@@ -0,0 +1,47 @@
+package stack;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * https://www.acmicpc.net/problem/4949
+ */
+public class No4949 {
+    public static List<Character> roundBrackets = Arrays.asList('(', ')');
+
+    public static List<Character> squareBrackets = Arrays.asList('[', ']');
+
+    public static void command(String command) {
+        System.out.println(isVPS(command) ? "yes" : "no");
+    }
+
+    private static boolean isVPS(String command) {
+        Stack<Character> stack = new Stack<>();
+        for (char character : command.toCharArray()) {
+            if (roundBrackets.contains(character)) {
+                if (character == '(')
+                    stack.push(character);
+                else if (stack.empty() || stack.pop() != '(')
+                    return false;
+            } else if (squareBrackets.contains(character)) {
+                if (character == '[')
+                    stack.push(character);
+                else if (stack.empty() || stack.pop() != '[')
+                    return false;
+            }
+        }
+        return stack.empty();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals("."))
+                break;
+            command(command);
+        }
+    }
+}
