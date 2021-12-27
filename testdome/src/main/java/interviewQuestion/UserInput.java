c1e91d7f8873f538c1d7b3abab0da21d74e17587
Owner <morningstar9047@gmail.com>
2021-12-27 15:24:21 +0900

    testDome: java-UserInput

---
+++ testdome/src/main/java/interviewQuestion/UserInput.java
@@ -0,0 +1,31 @@
+package interviewQuestion;
+
+public class UserInput {
+    public static class TextInput {
+        private final StringBuilder builder = new StringBuilder();
+
+        public void add(char c) {
+            builder.append(c);
+        }
+
+        public String getValue() {
+            return builder.toString();
+        }
+    }
+
+    public static class NumericInput extends TextInput {
+        @Override
+        public void add(char c) {
+            if (Character.isDigit(c))
+                super.add(c);
+        }
+    }
+
+    public static void main(String[] args) {
+        TextInput input = new NumericInput();
+        input.add('1');
+        input.add('a');
+        input.add('0');
+        System.out.println(input.getValue());
+    }
+}
