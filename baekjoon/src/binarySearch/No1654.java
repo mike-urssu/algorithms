3aa63da533988f2fdcd19b14da7f3bef57075fb2
Owner <morningstar9047@gmail.com>
2021-12-31 14:41:56 +0900

    백준: 이분 탐색-2805번 수정

--- baekjoon/src/binarySearch/No1654.java
+++ baekjoon/src/binarySearch/No1654.java
@@ -15,7 +15,7 @@ public class No1654 {
     public static int requiredWires;
     public static int[] wires;
     public static long high = Long.MAX_VALUE;
-    public static long low = 0;
+    public static long low;
 
     public static void setValues() throws IOException {
         tokenizer = new StringTokenizer(reader.readLine());
@@ -23,34 +23,31 @@ public class No1654 {
         requiredWires = Integer.parseInt(tokenizer.nextToken());
 
         wires = new int[numberOfWires];
-        for (int i = 0; i < numberOfWires; i++) {
+        for (int i = 0; i < numberOfWires; i++)
             wires[i] = Integer.parseInt(reader.readLine());
-            high = Math.max(high, wires[i]);
-        }
     }
 
-    public static long getMaxWires() {
-        long mid = low + (high - low) / 2;
+    public static long getMaxWireLength() {
         while (low + 1 < high) {
-            if (isValid(mid))
+            long mid = (low + high) / 2;
+            if (getWires(mid) >= requiredWires)
                 low = mid;
             else
                 high = mid;
-            mid = (high + low) / 2;
         }
         return low;
     }
 
-    private static boolean isValid(long mid) {
-        long count = 0;
-        for (int i = 0; i < numberOfWires; i++)
-            count += wires[i] / mid;
-        return count >= requiredWires;
+    private static int getWires(long mid) {
+        int count = 0;
+        for (int wire : wires)
+            count += wire / mid;
+        return count;
     }
 
     public static void main(String[] args) throws IOException {
         setValues();
-        writer.write(String.valueOf(getMaxWires()));
+        writer.write(String.valueOf(getMaxWireLength()));
         writer.flush();
     }
 }
