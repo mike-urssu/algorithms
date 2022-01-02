7cf3aa51225923c867506d0b7b5d1301f129f47a
Owner <morningstar9047@gmail.com>
2022-01-02 17:55:05 +0900

    코딩 테스트-하이퍼 커넥트 2번

---
+++ codingTest/src/main/java/com/testdome/service/HyperConnectApplication.java
@@ -0,0 +1,13 @@
+package com.testdome.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyperConnectApplication.class, args);
+    }
+
+}
---
+++ codingTest/src/test/java/com/testdome/service/HyperConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testdome.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
