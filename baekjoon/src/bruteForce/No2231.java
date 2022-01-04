2eb11a0f59feb4a3022772c3dc4e074aa285158b
Joon Hee Song <mike.urssu@gmail.com>
2022-01-04 10:16:13 +0900

    백준: 브루트 포스-2231번

---
+++ baekjoon/src/bruteForce/No2231.java
@@ -0,0 +1,33 @@
+package bruteForce;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class No2231 {
+    public static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static StringTokenizer tokenizer;
+
+    public static int getSubtotal(final int number) {
+        int smallestSubtotal = 0;
+        for (int i = number; i > 0; i--) {
+
+            int subtotal = i;
+
+            int temp = i;
+            while (temp > 0) {
+                subtotal += temp % 10;
+                temp /= 10;
+            }
+            if (subtotal == number)
+                smallestSubtotal = i;
+        }
+        return smallestSubtotal;
+    }
+
+    public static void main(String[] args) throws IOException {
+        int number = Integer.parseInt(reader.readLine());
+        writer.write(String.valueOf(getSubtotal(number)));
+        writer.flush();
+    }
+}
