f9bac10fa03d12a759ecc9d379d116cd58bf4f23
Joon Hee Song <mike.urssu@gmail.com>
2022-01-15 16:21:51 +0900

    백준: greedy-1541

---
+++ baekjoon-classification/src/greedy/No1541.kt
@@ -0,0 +1,36 @@
+package greedy
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+/**
+ * https://www.acmicpc.net/problem/1541
+ */
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
+    val expression = reader.readLine()
+
+    val numbers = expression.split('-', '+').map { number -> number.toInt() }
+    var index = 1
+
+    var isPositive = true
+    var sum = numbers[0]
+    for (i in expression.indices) {
+        if (expression[i] == '+') {
+            if (isPositive)
+                sum += numbers[index++]
+            else {
+                sum -= numbers[index++]
+                isPositive = false
+            }
+        } else if (expression[i] == '-') {
+            sum -= numbers[index++]
+            isPositive = false
+        }
+    }
+    writer.write(sum.toString())
+    writer.flush()
+}
