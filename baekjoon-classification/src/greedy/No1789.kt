8963d795c592b9ee403c33ebcb9200439a60825e
Joon Hee Song <mike.urssu@gmail.com>
2022-01-16 16:53:15 +0900

    백준: greedy-1789

---
+++ baekjoon-classification/src/greedy/No1789.kt
@@ -0,0 +1,22 @@
+package greedy
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+/**
+ * https://www.acmicpc.net/problem/1789
+ */
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
+    val number = reader.readLine().toLong()
+
+    var sum = 0L
+    var increment = 0
+    while (sum <= number)
+        sum += ++increment
+    writer.write((increment - 1).toString())
+    writer.flush()
+}
