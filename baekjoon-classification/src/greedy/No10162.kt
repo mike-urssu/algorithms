78d35f2d26ecab60672924c403a8c5a6b6827d97
Joon Hee Song <mike.urssu@gmail.com>
2022-01-16 14:09:20 +0900

    백준: greedy-10162

---
+++ baekjoon-classification/src/greedy/No10162.kt
@@ -0,0 +1,29 @@
+package greedy
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+/**
+ * https://www.acmicpc.net/problem/10162
+ */
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+    var second = reader.readLine().toInt()
+    if (second % 10 != 0) {
+        writer.write((-1).toString())
+        writer.flush()
+        return
+    }
+
+    val timers = arrayOf(300, 60, 10)
+    for (timer in timers) {
+        writer.write("${second / timer} ")
+        second %= timer
+    }
+
+    writer.flush()
+}
