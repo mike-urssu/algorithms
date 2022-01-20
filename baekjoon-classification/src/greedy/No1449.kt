c330068d37b740e48951077dad692f94035a7b46
Joon Hee Song <mike.urssu@gmail.com>
2022-01-20 11:43:31 +0900

    백준: greedy-1449

---
+++ baekjoon-classification/src/greedy/No1449.kt
@@ -0,0 +1,33 @@
+package greedy
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+/**
+ * https://www.acmicpc.net/problem/1449
+ */
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+    val numbers = reader.readLine().split(' ').map { it.toInt() }
+    val length = numbers[1]
+    val leaks = reader.readLine().split(' ').map { it.toInt() }.sorted()
+
+    writer.write(getMinimumTapeCount(length, leaks).toString())
+    writer.flush()
+}
+
+fun getMinimumTapeCount(length: Int, leaks: List<Int>): Int {
+    var count = 0
+    var lastLeakPoint = 0
+    for (leak in leaks) {
+        if (lastLeakPoint <= leak) {
+            lastLeakPoint = leak + length
+            count++
+        }
+    }
+    return count
+}
