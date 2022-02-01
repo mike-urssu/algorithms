92f7433c8a6665668315e5270785293e35f6b7dd
Joon Hee Song <mike.urssu@gmail.com>
2022-02-01 11:46:53 +0900

    백준: 정렬-2776 다른 풀이

--- baekjoon-classification/src/sorting/No2776.kt
+++ baekjoon-classification/src/sorting/No2776.kt
@@ -15,18 +15,48 @@ fun main() {
     val testCase = reader.readLine().toInt()
     for (i in 0 until testCase) {
         reader.readLine().toInt()
-        val numbers = HashSet<Int>()
-        reader.readLine().split(' ').map { numbers.add(it.toInt()) }
-        reader.readLine()
+        val numbers = reader.readLine().split(' ').map { it.toInt() }.toIntArray().sorted()
+        reader.readLine().toInt()
         val numbers2 = reader.readLine().split(' ').map { it.toInt() }.toIntArray()
-
         for (number in numbers2) {
-            if (numbers.contains(number))
+            if (binarySearch(numbers, number))
                 writer.write("1")
             else
                 writer.write("0")
             writer.newLine()
         }
+
+//        reader.readLine().toInt()
+//        val numbers = HashSet<Int>()
+//        reader.readLine().split(' ').map { numbers.add(it.toInt()) }
+//        reader.readLine()
+//        val numbers2 = reader.readLine().split(' ').map { it.toInt() }.toIntArray()
+//
+//        for (number in numbers2) {
+//            if (numbers.contains(number))
+//                writer.write("1")
+//            else
+//                writer.write("0")
+//            writer.newLine()
+//        }
     }
     writer.flush()
+}
+
+fun binarySearch(numbers: List<Int>, number: Int): Boolean {
+    if (number !in numbers[0]..numbers[numbers.size - 1])
+        return false
+
+    var low = 0
+    var high = numbers.size - 1
+    while (low + 1 < high) {
+        val mid = (low + high) / 2
+        if (number < numbers[mid])
+            high = mid
+        else if (number == numbers[mid])
+            return true
+        else
+            low = mid
+    }
+    return numbers[low] == number || numbers[high] == number
 }
