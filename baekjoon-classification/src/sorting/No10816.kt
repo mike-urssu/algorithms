0798a666740452cd0d890335a83463f40b274daf
Joon Hee Song <mike.urssu@gmail.com>
2022-02-05 13:50:10 +0900

    백준: sorting-10816 다른 풀이

--- baekjoon-classification/src/sorting/No10816.kt
+++ baekjoon-classification/src/sorting/No10816.kt
@@ -17,6 +17,47 @@ fun main() {
     reader.readLine()
     val numbers = reader.readLine().split(' ').map { it.toInt() }
 
+    for (number in numbers)
+        writer.write("${getUpperBound(cards, number) - getLowerBound(cards, number)} ")
+    writer.flush()
+}
+
+private fun getUpperBound(cards: List<Int>, number: Int): Int {
+    var low = -1
+    var high = cards.size
+    while (low + 1 < high) {
+        val mid = (low + high) / 2
+        if (cards[mid] <= number)
+            low = mid
+        else
+            high = mid
+    }
+    return high
+}
+
+private fun getLowerBound(cards: List<Int>, number: Int): Int {
+    var low = -1
+    var high = cards.size
+    while (low + 1 < high) {
+        val mid = (low + high) / 2
+        if (cards[mid] >= number)
+            high = mid
+        else
+            low = mid
+    }
+    return high
+}
+
+/*
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+    reader.readLine().toInt()
+    val cards = reader.readLine().split(' ').map { it.toInt() }.sorted()
+    reader.readLine()
+    val numbers = reader.readLine().split(' ').map { it.toInt() }
+
     val countOfCards = HashMap<Int, Int>()
     for (card in cards)
         countOfCards[card] = countOfCards.getOrDefault(card, 0) + 1
@@ -25,3 +66,4 @@ fun main() {
         writer.write("${countOfCards.getOrDefault(number, 0)} ")
     writer.flush()
 }
+*/
