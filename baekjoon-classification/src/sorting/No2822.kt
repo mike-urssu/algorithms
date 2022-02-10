34591a9504905e694ec95bc1daecca27e2ef52d9
Joon Hee Song <mike.urssu@gmail.com>
2022-02-10 10:15:00 +0900

    BaekJoon: Sorting-2822 다른 풀이

--- baekjoon-classification/src/sorting/No2822.kt
+++ baekjoon-classification/src/sorting/No2822.kt
@@ -4,13 +4,43 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.util.*
-import kotlin.collections.HashSet
 
 /**
  * https://www.acmicpc.net/problem/2822
  */
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
 
+    val scores = IntArray(8)
+    for (i in scores.indices) {
+        val score = reader.readLine().toInt()
+        scores[i] = score
+    }
+
+    val highestScores = scores.sortedArrayDescending().copyOf(5)
+
+    val indices = IntArray(5)
+    var index = 0
+    for (i in scores.indices) {
+        val score = scores[i]
+        for (j in 0 until 5) {
+            val highestScore = highestScores[j]
+            if (score == highestScore) {
+                indices[index++] = i + 1
+                break
+            }
+        }
+    }
+
+    writer.write(highestScores.sum().toString())
+    writer.newLine()
+    for (i in indices)
+        writer.write("$i ")
+    writer.flush()
+}
+
+/*
 fun main() {
     val reader = BufferedReader(InputStreamReader(System.`in`))
     val writer = BufferedWriter(OutputStreamWriter(System.out))
@@ -39,3 +69,4 @@ fun main() {
     }
     writer.flush()
 }
+*/
