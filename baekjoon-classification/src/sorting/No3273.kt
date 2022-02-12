9f061151664ec63400857051bf79bc81cc4a4c7f
Joon Hee Song <mike.urssu@gmail.com>
2022-02-12 14:06:47 +0900

    BaekJoon: Sorting-3273

---
+++ baekjoon-classification/src/sorting/No3273.kt
@@ -0,0 +1,36 @@
+package sorting
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+/**
+ * https://www.acmicpc.net/problem/3273
+ */
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+    reader.readLine()
+    val numbers = reader.readLine().split(' ').map { it.toInt() }.sorted()
+    val target = reader.readLine().toInt()
+
+    var count = 0
+    var i = 0
+    var j = numbers.size - 1
+    while (i < j) {
+        val sum = numbers[i] + numbers[j]
+        if (sum == target) {
+            i++
+            j--
+            count++
+        } else if (target < sum)
+            j--
+        else
+            i++
+    }
+
+    writer.write(count.toString())
+    writer.flush()
+}
