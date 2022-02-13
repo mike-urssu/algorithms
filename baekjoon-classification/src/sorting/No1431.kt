e91fe44f0af3b72d6a4684fd64a917fe9437e30b
Joon Hee Song <mike.urssu@gmail.com>
2022-02-13 13:47:56 +0900

    BaekJoon: Sorting-1431

---
+++ baekjoon-classification/src/sorting/No1431.kt
@@ -0,0 +1,48 @@
+package sorting
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+/**
+ * https://www.acmicpc.net/problem/1431
+ */
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+    val numberOfInput = reader.readLine().toInt()
+    val serialNumbers = Array(numberOfInput) { "" }
+    for (i in 0 until numberOfInput)
+        serialNumbers[i] = reader.readLine()
+
+    val comparator = Comparator<String> { it, other ->
+        if (it.length != other.length)
+            it.length.compareTo(other.length)
+        else {
+            val sumOfIt = getSumOfDigit(it)
+            val sumOfOther = getSumOfDigit(other)
+            if (sumOfIt == sumOfOther)
+                it.compareTo(other)
+            else
+                sumOfIt - sumOfOther
+        }
+    }
+    serialNumbers.sortWith(comparator)
+
+    for (serialNumber in serialNumbers) {
+        writer.write(serialNumber)
+        writer.newLine()
+    }
+    writer.flush()
+}
+
+private fun getSumOfDigit(serialNumber: String): Int {
+    var sum = 0
+    for (character in serialNumber) {
+        if (character.isDigit())
+            sum += character.digitToInt()
+    }
+    return sum
+}
