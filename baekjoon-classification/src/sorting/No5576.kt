74c9d63dae37b83172809590049cdde46f506e96
Joon Hee Song <mike.urssu@gmail.com>
2022-02-14 10:05:04 +0900

    BaekJoon: Sorting-5576

---
+++ baekjoon-classification/src/sorting/No5576.kt
@@ -0,0 +1,29 @@
+package sorting
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+/**
+ * https://www.acmicpc.net/problem/5576
+ */
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+    writer.write("${getScore(reader)} ${getScore(reader)}")
+    writer.flush()
+}
+
+private fun getScore(reader: BufferedReader): Int {
+    val scores = IntArray(10)
+    for (i in 0 until 10)
+        scores[i] = reader.readLine().toInt()
+    scores.sortDescending()
+
+    var sum = 0
+    for (i in 0 until 3)
+        sum += scores[i]
+    return sum
+}
