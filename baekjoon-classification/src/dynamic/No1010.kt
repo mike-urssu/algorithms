969f1d67665150fea7dd4c9f5ae841b013e6ce3f
Joon Hee Song <mike.urssu@gmail.com>
2022-02-24 22:20:42 +0900

    BaekJoon: Dynamic-1010 다른 풀이

--- baekjoon-classification/src/dynamic/No1010.kt
+++ baekjoon-classification/src/dynamic/No1010.kt
@@ -4,13 +4,37 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.math.BigInteger
 
 /**
  * https://www.acmicpc.net/problem/1010
  */
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val numberOfInput = readLine().toInt()
+    val dp = Array(30) { IntArray(30) }
+    for (i in dp.indices) {
+        dp[i][0] = 1
+        dp[i][i] = 1
+    }
+
+    for (i in 2 until dp.size) {
+        for (j in 1 until dp.size)
+            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+    }
+
+    repeat(numberOfInput) {
+        val numbers = readLine().split(' ').map { it.toInt() }
+        with(BufferedWriter(OutputStreamWriter(System.out))) {
+            write("${dp[numbers[1]][numbers[0]]}")
+            newLine()
+            flush()
+        }
+    }
+}
+
+/*
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val numberOfInput = readLine().toInt()
+
     repeat(numberOfInput) {
         val numbers = readLine().split(' ').map { it.toInt() }
 
@@ -29,3 +53,4 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
         }
     }
 }
+*/
