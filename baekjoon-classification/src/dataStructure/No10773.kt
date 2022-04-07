3b1c434ddd15da8fbf7bed63efddcaae33f99d3f
Joon Hee Song <mike.urssu@gmail.com>
2022-04-07 11:14:51 +0900

    BaekJoon: 자료구조-10773

---
+++ baekjoon-classification/src/dataStructure/No10773.kt
@@ -0,0 +1,31 @@
+package dataStructure
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+/**
+ * https://www.acmicpc.net/problem/10773
+ */
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
+    val stack = Stack<Int>()
+
+    val numberOfInput = reader.readLine().toInt()
+    repeat(numberOfInput) {
+        val number = reader.readLine().toInt()
+        if (number == 0)
+            stack.pop()
+        else
+            stack.push(number)
+    }
+
+    var sum = 0
+    while (!stack.isEmpty())
+        sum += stack.pop()
+    writer.write("$sum")
+    writer.flush()
+}
