82a5652305062d3b27e0620dffb0181b191252cb
Joon Hee Song <mike.urssu@gmail.com>
2022-04-07 12:18:51 +0900

    Programmers(level2): 문자열 압축 내부 로직 분리

--- programmers/src/level2/StringCompression.kt
+++ programmers/src/level2/StringCompression.kt
@@ -1,30 +1,24 @@
 package level2
 
 import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * https://programmers.co.kr/learn/courses/30/lessons/60057
  */
 class StringCompression {
+    private lateinit var substrings: LinkedList<String>
+    private lateinit var counts: ArrayList<Int>
+
     fun solution(string: String): Int {
         var length = string.length
-        for (i in 1..length / 2)
-            length = length.coerceAtMost(getLength(string, i))
+        for (lengthOfSubstring in 1..length / 2)
+            length = length.coerceAtMost(getLength(string, lengthOfSubstring))
         return length
     }
 
-    private fun getLength(string: String, lengthOfSlice: Int): Int {
-        val substrings = LinkedList<String>()
-        val counts = arrayListOf<Int>()
-
-        for (i in 0 until string.length / lengthOfSlice) {
-            val substring = string.substring(i * lengthOfSlice, (i + 1) * lengthOfSlice)
-            if (substrings.peekLast() != substring) {
-                substrings.add(substring)
-                counts.add(1)
-            } else
-                counts[counts.size - 1]++
-        }
+    private fun getLength(string: String, lengthOfSubstring: Int): Int {
+        compressString(string, lengthOfSubstring)
 
         val builder = StringBuilder()
         for (i in counts.indices) {
@@ -32,10 +26,26 @@ class StringCompression {
                 builder.append(counts[i])
             builder.append(substrings.pop())
         }
-        if (string.length > string.length / lengthOfSlice * lengthOfSlice)
-            builder.append(string.substring((string.length / lengthOfSlice) * lengthOfSlice))
+
+        val currentIndexOfString = string.length / lengthOfSubstring * lengthOfSubstring
+        if (string.length > currentIndexOfString)
+            builder.append(string.substring(currentIndexOfString))
 
         return builder.length
+    }
+
+    private fun compressString(string: String, lengthOfSubstring: Int) {
+        substrings = LinkedList()
+        counts = arrayListOf()
+
+        for (i in 0 until string.length / lengthOfSubstring) {
+            val substring = string.substring(i * lengthOfSubstring, (i + 1) * lengthOfSubstring)
+            if (substrings.peekLast() != substring) {
+                substrings.add(substring)
+                counts.add(1)
+            } else
+                counts[counts.size - 1]++
+        }
     }
 }
 
