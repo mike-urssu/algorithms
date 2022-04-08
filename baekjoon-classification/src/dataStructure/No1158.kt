29a40b35e3dc09559c1c63a2cff91a70f71998f0
Joon Hee Song <mike.urssu@gmail.com>
2022-04-08 09:35:19 +0900

    BaekJoon: 자료구조-1158

---
+++ baekjoon-classification/src/dataStructure/No1158.kt
@@ -0,0 +1,32 @@
+package dataStructure
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.LinkedList
+
+/**
+ * https://www.acmicpc.net/problem/1158
+ */
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
+    val queue = LinkedList<Int>()
+
+    val numbers = reader.readLine().split(" ").map { it.toInt() }
+    for (i in 1..numbers[0])
+        queue.add(i)
+
+    val josephus = IntArray(numbers[0])
+    var index = 0
+    while (queue.isNotEmpty()) {
+        repeat(numbers[1] - 1) {
+            queue.add(queue.pop())
+        }
+        josephus[index++] = queue.pop()
+    }
+
+    writer.write(josephus.joinToString(", ", "<", ">"))
+    writer.flush()
+}
