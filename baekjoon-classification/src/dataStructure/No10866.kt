6262e007580061c71208527e74fb4240dd1edb6d
Joon Hee Song <mike.urssu@gmail.com>
2022-04-08 10:57:13 +0900

    BaekJoon: 자료구조-10866

---
+++ baekjoon-classification/src/dataStructure/No10866.kt
@@ -0,0 +1,45 @@
+package dataStructure
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.LinkedList
+
+/**
+ * https://www.acmicpc.net/problem/10866
+ */
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
+    val deque = LinkedList<Int>()
+
+    val numberOfInput = reader.readLine().toInt()
+    repeat(numberOfInput) {
+        val operations = reader.readLine().split(" ")
+        if (operations[0] == "push_front")
+            deque.addFirst(operations[1].toInt())
+        else if (operations[0] == "push_back")
+            deque.addLast(operations[1].toInt())
+        else if (operations[0] == "pop_front") {
+            writer.write("${if (deque.isEmpty()) -1 else deque.pollFirst()}")
+            writer.newLine()
+        } else if (operations[0] == "pop_back") {
+            writer.write("${if (deque.isEmpty()) -1 else deque.pollLast()}")
+            writer.newLine()
+        } else if (operations[0] == "size") {
+            writer.write("${deque.size}")
+            writer.newLine()
+        } else if (operations[0] == "empty") {
+            writer.write("${if (deque.isEmpty()) 1 else 0}")
+            writer.newLine()
+        } else if (operations[0] == "front") {
+            writer.write("${if (deque.isEmpty()) -1 else deque.peekFirst()}")
+            writer.newLine()
+        } else if (operations[0] == "back") {
+            writer.write("${if (deque.isEmpty()) -1 else deque.peekLast()}")
+            writer.newLine()
+        }
+    }
+    writer.flush()
+}
