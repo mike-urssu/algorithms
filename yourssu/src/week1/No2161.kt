63c2bdcd7222f901dee6c4d5e4ec758721a3f0ae
Joon Hee Song <mike.urssu@gmail.com>
2022-08-07 11:12:47 +0900

    Baekjoon: No2161 Queue Interface로 구현

--- yourssu/src/week1/No2161.kt
+++ yourssu/src/week1/No2161.kt
@@ -5,6 +5,7 @@ import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.util.LinkedList
+import java.util.Queue
 
 /**
  * https://www.acmicpc.net/problem/2161
@@ -14,13 +15,13 @@ private val reader = BufferedReader(InputStreamReader(System.`in`))
 private val writer = BufferedWriter(OutputStreamWriter(System.out))
 
 fun main() {
-    val queue = LinkedList<Int>()
+    val queue: Queue<Int> = LinkedList()
     val number = reader.readLine().toInt()
     repeat(number) { queue.add(it + 1) }
 
     while (queue.size != 1) {
-        writer.write("${queue.pollFirst()} ")
-        queue.add(queue.pollFirst())
+        writer.write("${queue.poll()} ")
+        queue.add(queue.poll())
     }
     writer.write("${queue.poll()}")
 
