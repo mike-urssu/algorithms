ede7828192ec94663a4150cf555c71a27a9d6387
Joon Hee Song <mike.urssu@gmail.com>
2022-08-07 10:45:20 +0900

    BaekJoon: No10871

---
+++ baekjoon/src/yourssu/week1/No10871.kt
@@ -0,0 +1,18 @@
+package yourssu.week1
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+private val reader = BufferedReader(InputStreamReader(System.`in`))
+private val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+fun main() {
+    val givenNumber = reader.readLine().split(" ").map { it.toInt() }[1]
+    reader.readLine().split(" ").forEach { number ->
+        if (number.toInt() < givenNumber)
+            writer.write("$number ")
+    }
+    writer.flush()
+}
