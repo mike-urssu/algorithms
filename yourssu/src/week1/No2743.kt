296dd762d0a6a0dfe5c68fd6f1ae21690bfec087
Joon Hee Song <mike.urssu@gmail.com>
2022-08-07 11:29:51 +0900

    BaekJoon: No2743

---
+++ yourssu/src/week1/No2743.kt
@@ -0,0 +1,18 @@
+package week1
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+/**
+ * https://www.acmicpc.net/problem/2743
+ */
+
+private val reader = BufferedReader(InputStreamReader(System.`in`))
+private val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+fun main() {
+    writer.write(reader.readLine().length.toString())
+    writer.flush()
+}
