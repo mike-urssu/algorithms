781dcd8bddfeb00871acbac3107fd0d615dffc0b
Joon Hee Song <mike.urssu@gmail.com>
2022-08-07 11:06:28 +0900

    No10828 import문 * 제거

--- yourssu/src/week1/No10828.kt
+++ yourssu/src/week1/No10828.kt
@@ -4,7 +4,7 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.util.*
+import java.util.Stack
 
 /**
  * https://www.acmicpc.net/problem/10828
