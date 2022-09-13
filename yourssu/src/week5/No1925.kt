2566da502839a875aa8b175049b514af76346af7
Joon Hee Song <mike.urssu@gmail.com>
2022-09-13 09:34:51 +0900

    BaekJoon: No1925 (다른 풀이)

--- yourssu/src/week5/No1925.kt
+++ yourssu/src/week5/No1925.kt
@@ -4,7 +4,6 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import kotlin.math.pow
 
 /**
  * https://www.acmicpc.net/problem/1925
@@ -17,27 +16,30 @@ fun main() {
     val p2 = reader.readLine().split(" ").map { it.toInt() }
     val p3 = reader.readLine().split(" ").map { it.toInt() }
 
-    val lengthP12 = (p2[0] - p1[0]).toDouble().pow(2).toInt() + (p2[1] - p1[1]).toDouble().pow(2).toInt()
-    val lengthP23 = (p3[0] - p2[0]).toDouble().pow(2).toInt() + (p3[1] - p2[1]).toDouble().pow(2).toInt()
-    val lengthP31 = (p1[0] - p3[0]).toDouble().pow(2).toInt() + (p1[1] - p3[1]).toDouble().pow(2).toInt()
+    val ratioOfCos = getRatioOfCos(p1, p2, p3)
 
     if (isX(p1, p2, p3))
         writer.write("X")
-    else if (isJungTriangle(lengthP12, lengthP23, lengthP31))
+    else if (isJungTriangle(ratioOfCos))
         writer.write("JungTriangle")
     else {
-        val signsOfDegree = getSignsOfDegree(p1, p2, p3)
-
-        if (signsOfDegree.any { it < 0 }) {
-            writer.write(if (isTwoLengthSame(lengthP12, lengthP23, lengthP31)) "Dunkak2Triangle" else "DunkakTriangle")
-        } else if (signsOfDegree.any { it == 0 }) {
-            writer.write(if (isTwoLengthSame(lengthP12, lengthP23, lengthP31)) "Jikkak2Triangle" else "JikkakTriangle")
-        } else {
-            writer.write(if (isTwoLengthSame(lengthP12, lengthP23, lengthP31)) "Yeahkak2Triangle" else "YeahkakTriangle")
-        }
+        if (ratioOfCos.any { it < 0 })
+            writer.write(if (isTwoRatioOfCosSame(ratioOfCos)) "Dunkak2Triangle" else "DunkakTriangle")
+        else if (ratioOfCos.any { it == 0 })
+            writer.write(if (isTwoRatioOfCosSame(ratioOfCos)) "Jikkak2Triangle" else "JikkakTriangle")
+        else
+            writer.write(if (isTwoRatioOfCosSame(ratioOfCos)) "Yeahkak2Triangle" else "YeahkakTriangle")
     }
 
     writer.flush()
+}
+
+private fun getRatioOfCos(p1: List<Int>, p2: List<Int>, p3: List<Int>): IntArray {
+    val signs = IntArray(3)
+    signs[0] = (p2[0] - p1[0]) * (p3[0] - p1[0]) + (p2[1] - p1[1]) * (p3[1] - p1[1])
+    signs[1] = (p1[0] - p2[0]) * (p3[0] - p2[0]) + (p1[1] - p2[1]) * (p3[1] - p2[1])
+    signs[2] = (p1[0] - p3[0]) * (p2[0] - p3[0]) + (p1[1] - p3[1]) * (p2[1] - p3[1])
+    return signs
 }
 
 private fun isX(p1: List<Int>, p2: List<Int>, p3: List<Int>): Boolean {
@@ -49,18 +51,10 @@ private fun isX(p1: List<Int>, p2: List<Int>, p3: List<Int>): Boolean {
         (p2[0] - p1[0]) * (p3[1] - p1[1]) == (p3[0] - p1[0]) * (p2[1] - p1[1])
 }
 
-private fun isJungTriangle(lengthP12: Int, lengthP23: Int, lengthP31: Int): Boolean {
-    return lengthP12 == lengthP23 && lengthP23 == lengthP31
+private fun isJungTriangle(ratioOfCos: IntArray): Boolean {
+    return ratioOfCos[0] == ratioOfCos[1] && ratioOfCos[1] == ratioOfCos[2]
 }
 
-private fun getSignsOfDegree(p1: List<Int>, p2: List<Int>, p3: List<Int>): IntArray {
-    val signs = IntArray(3)
-    signs[0] = (p2[0] - p1[0]) * (p3[0] - p1[0]) + (p2[1] - p1[1]) * (p3[1] - p1[1])
-    signs[1] = (p1[0] - p2[0]) * (p3[0] - p2[0]) + (p1[1] - p2[1]) * (p3[1] - p2[1])
-    signs[2] = (p1[0] - p3[0]) * (p2[0] - p3[0]) + (p1[1] - p3[1]) * (p2[1] - p3[1])
-    return signs
-}
-
-private fun isTwoLengthSame(lengthP12: Int, lengthP23: Int, lengthP31: Int): Boolean {
-    return lengthP12 == lengthP23 || lengthP23 == lengthP31 || lengthP31 == lengthP12
+private fun isTwoRatioOfCosSame(ratioOfCos: IntArray): Boolean {
+    return ratioOfCos[0] == ratioOfCos[1] || ratioOfCos[1] == ratioOfCos[2] || ratioOfCos[2] == ratioOfCos[0]
 }
