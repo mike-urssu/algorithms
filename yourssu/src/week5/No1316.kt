236da6c76286bdc821534caf06ae42da9f7ed2d5
Joon Hee Song <mike.urssu@gmail.com>
2022-09-18 10:20:35 +0900

    BaekJoon: No1316

---
+++ yourssu/src/week5/No1316.kt
@@ -0,0 +1,40 @@
+package week5
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+/**
+ * https://www.acmicpc.net/problem/1316
+ */
+private val reader = BufferedReader(InputStreamReader(System.`in`))
+private val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+fun main() {
+    var count = 0
+    val numberOfInput = reader.readLine().toInt()
+    repeat(numberOfInput) {
+        val word = reader.readLine()
+        if (isWordGroup(word))
+            count++
+    }
+    writer.write("$count")
+    writer.flush()
+}
+
+private fun isWordGroup(word: String): Boolean {
+    val characters = mutableSetOf<Char>()
+
+    var previousCharacter = word[0]
+    characters.add(previousCharacter)
+    for (i in 1 until word.length) {
+        if (characters.contains(word[i]) && previousCharacter != word[i])
+            return false
+        else {
+            characters.add(word[i])
+            previousCharacter = word[i]
+        }
+    }
+    return true
+}
