c13c12d3f0c30cf452436aa9e13a495c6212b89e
Joon Hee Song <mike.urssu@gmail.com>
2022-09-18 11:55:15 +0900

    BaekJoon: No10953

---
+++ yourssu/src/week5/No10953.kt
@@ -0,0 +1,20 @@
+package week5
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+/**
+ * https://www.acmicpc.net/problem/10953
+ */
+private val reader = BufferedReader(InputStreamReader(System.`in`))
+private val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+fun main() {
+    val numberOfInput = reader.readLine().toInt()
+    repeat(numberOfInput) {
+        writer.write("${reader.readLine().split(",").sumOf { it.toInt() }}\n")
+    }
+    writer.flush()
+}
