f13fdf5a2c9883800ac3137ea8c2da5064204cb9
Joon Hee Song <mike.urssu@gmail.com>
2022-10-04 15:55:47 +0900

    BaekJoon: No1010

--- baekjoon/src/silver5/No1010.kt
+++ baekjoon/src/silver5/No1010.kt
@@ -4,31 +4,33 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.math.BigInteger
 
 /**
  * https://www.acmicpc.net/problem/1010
  */
 private val reader = BufferedReader(InputStreamReader(System.`in`))
 private val writer = BufferedWriter(OutputStreamWriter(System.out))
+private lateinit var graph: Array<IntArray>
 
 fun main() {
+    setGraph()
+
     val numberOfInput = reader.readLine().toInt()
     repeat(numberOfInput) {
-        val numbers = reader.readLine().split(" ").map { it.toLong() }
-        writer.write("${combination(numbers[1], numbers[0].coerceAtMost(numbers[1] - numbers[0]))}\n")
+        val numbers = reader.readLine().split(" ").map { it.toInt() }
+        writer.write("${graph[numbers[1]][numbers[0]]}\n")
     }
     writer.flush()
 }
 
-private fun combination(number1: Long, number2: Long): BigInteger {
-    return factorial(BigInteger.valueOf(number1)) /
-            (factorial(BigInteger.valueOf(number1 - number2)) * factorial(BigInteger.valueOf(number2)))
-}
-
-private fun factorial(number: BigInteger): BigInteger {
-    return if (number == BigInteger.ZERO || number == BigInteger.ONE)
-        BigInteger.ONE
-    else
-        number * factorial(number.minus(BigInteger.ONE))
+private fun setGraph() {
+    graph = Array(31) { IntArray(31) }
+    for (i in 0..30) {
+        for (j in 0..i) {
+            if (j == 0 || j == i)
+                graph[i][j] = 1
+            else
+                graph[i][j] = graph[i - 1][j - 1] + graph[i - 1][j]
+        }
+    }
 }
