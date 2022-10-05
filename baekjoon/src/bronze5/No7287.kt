5dfe6484e8d5dfa3996267a51c93475caa993638
Joon Hee Song <mike.urssu@gmail.com>
2022-10-05 10:07:00 +0900

    BaekJoon: No7287

---
+++ baekjoon/src/bronze5/No7287.kt
@@ -0,0 +1,15 @@
+package bronze5
+
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+
+/**
+ * https://www.acmicpc.net/problem/7287
+ */
+private val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+fun main() {
+    writer.write("294\n")
+    writer.write("morningstar9047")
+    writer.flush()
+}
