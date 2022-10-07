b178374c4b15d67b2b196f9550992fa0a5d5ccf7
Joon Hee Song <mike.urssu@gmail.com>
2022-10-07 14:56:29 +0900

    BaekJoon: No1052

---
+++ baekjoon/src/silver1/No1052.kt
@@ -0,0 +1,24 @@
+package silver1
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+/**
+ * https://www.acmicpc.net/problem/1052
+ */
+private val reader = BufferedReader(InputStreamReader(System.`in`))
+private val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+fun main() {
+    val numbers = reader.readLine().split(" ").map { it.toInt() }
+
+    var sum = numbers[0]
+    while (sum.countOneBits() > numbers[1]) {
+        sum++
+    }
+
+    writer.write("${sum - numbers[0]}")
+    writer.flush()
+}
