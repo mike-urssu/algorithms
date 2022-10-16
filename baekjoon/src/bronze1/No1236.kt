4680cd29149a07b763cbe326d1e3f1cbada2740b
Joon Hee Song <mike.urssu@gmail.com>
2022-10-16 18:40:18 +0900

    BaekJoon: No1236 다른 풀이

--- baekjoon/src/bronze1/No1236.kt
+++ baekjoon/src/bronze1/No1236.kt
@@ -11,50 +11,31 @@ import java.io.OutputStreamWriter
 private val reader = BufferedReader(InputStreamReader(System.`in`))
 private val writer = BufferedWriter(OutputStreamWriter(System.out))
 
-private lateinit var graph: Array<CharArray>
-private lateinit var guardsInCol: BooleanArray
-
 fun main() {
     val size = reader.readLine().split(" ").map { it.toInt() }
-    setGraph(size)
-    setGuardsInCol(size)
+    val graph = Array(size[0]) { reader.readLine().toCharArray() }
 
-    var count = 0
-    for (i in 0 until size[0]) {
-        if (graph[i].all { it == '.' }) {
-            for (j in size[1] - 1 downTo 0) {
-                if (guardsInCol.all { it }) {
-                    graph[i][0] = 'X'
-                    count++
-                    break
-                } else if (!guardsInCol[j]) {
-                    guardsInCol[j] = true
-                    graph[i][j] = 'X'
-                    count++
-                    break
-                }
-            }
+    var guardsNotInRow = 0
+    graph.forEach { row ->
+        if (row.all { it == '.' }) {
+            guardsNotInRow++
         }
     }
 
-    count += guardsInCol.count { !it }
-
-    writer.write("$count")
-    writer.flush()
-}
-
-private fun setGraph(size: List<Int>) {
-    graph = Array(size[0]) { reader.readLine().toCharArray() }
-}
-
-private fun setGuardsInCol(size: List<Int>) {
-    guardsInCol = BooleanArray(size[1])
+    var guardsNotInCol = 0
     for (j in 0 until size[1]) {
+        var isColContainsGuard = false
         for (i in 0 until size[0]) {
             if (graph[i][j] == 'X') {
-                guardsInCol[j] = true
+                isColContainsGuard = true
                 break
             }
         }
+        if (!isColContainsGuard) {
+            guardsNotInCol++
+        }
     }
+
+    writer.write("${guardsNotInRow.coerceAtLeast(guardsNotInCol)}")
+    writer.flush()
 }
