2b1f690a8e92debe46890d9954979aa1af727364
Joon Hee Song <mike.urssu@gmail.com>
2022-10-24 09:21:11 +0900

    BaekJoon: No10172

---
+++ baekjoon/src/bronze5/No10172.kt
@@ -0,0 +1,16 @@
+package bronze5
+
+/**
+ * https://www.acmicpc.net/problem/10172
+ */
+fun main() {
+    println(
+        """
+|\_/|
+|q p|   /}
+( 0 )""${'"'}\
+|"^"`    |
+||_/=\\__|
+    """.trimIndent()
+    )
+}
