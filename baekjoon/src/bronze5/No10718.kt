231242258cc70c69cf6fec87b514c5f0838282eb
Joon Hee Song <mike.urssu@gmail.com>
2022-10-26 13:48:20 +0900

    BaekJoon: No10718

---
+++ baekjoon/src/bronze5/No10718.kt
@@ -0,0 +1,13 @@
+package bronze5
+
+/**
+ * https://www.acmicpc.net/problem/10718
+ */
+fun main() {
+    println(
+        """
+        강한친구 대한육군
+        강한친구 대한육군
+        """.trimIndent()
+    )
+}
