3436476a05ddd2ad4b48f5dc3a26a61ebfca8e6c
Joon Hee Song <mike.urssu@gmail.com>
2022-10-26 13:50:05 +0900

    BaekJoon: No10757

---
+++ baekjoon/src/bronze5/No10757.kt
@@ -0,0 +1,17 @@
+package bronze5
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+/**
+ * https://www.acmicpc.net/problem/10757
+ */
+private val reader = BufferedReader(InputStreamReader(System.`in`))
+private val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+fun main() {
+    writer.write("${reader.readLine().split(" ").map { it.toBigInteger() }.sumOf { it }}")
+    writer.flush()
+}
