0b2dbd2ebe437f5d48284a100ad2a3c85a365981
Joon Hee Song <mike.urssu@gmail.com>
2022-11-04 23:43:58 +0900

    BaekJoon: No16394

---
+++ baekjoon/src/bronze5/No16394.kt
@@ -0,0 +1,11 @@
+package bronze5
+
+/**
+ * https://www.acmicpc.net/problem/16394
+ */
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    println(scanner.nextInt() - 1946)
+}
