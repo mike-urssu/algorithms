1e3a0d524121b89a5393fe8ac6477182b4cc53c3
mike-urssu <mike.urssu@gmail.com>
2023-01-31 10:27:44 +0900

    BaekJoon: No4470

---
+++ baekjoon/src/bronze4/No4470.kt
@@ -0,0 +1,20 @@
+package bronze4
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+/**
+ * https://www.acmicpc.net/problem/4470
+ */
+private val reader = BufferedReader(InputStreamReader(System.`in`))
+private val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+fun main() {
+    val numberOfInput = reader.readLine().toInt()
+    repeat(numberOfInput) { i ->
+        writer.write("${i + 1}. ${reader.readLine()}\n")
+    }
+    writer.flush()
+}
