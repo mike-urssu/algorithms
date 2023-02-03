d78944f229bbe6d4cc5b8d79109730f78823cac2
mike-urssu <mike.urssu@gmail.com>
2023-02-03 09:32:52 +0900

    BaekJoon: No2441

---
+++ baekjoon/src/bronze3/No2441.kt
@@ -0,0 +1,21 @@
+package bronze3
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+/**
+ * https://www.acmicpc.net/problem/2441
+ */
+private val reader = BufferedReader(InputStreamReader(System.`in`))
+private val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+fun main() {
+    val number = reader.readLine().toInt()
+    for (i in 0 until number) {
+        writer.write(" ".repeat(i))
+        writer.write("${"*".repeat(number - i)}\n")
+    }
+    writer.flush()
+}
