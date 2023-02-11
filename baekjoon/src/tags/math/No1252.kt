812f13042c9cfe8f34edd00b082af61261062718
mike-urssu <mike.urssu@gmail.com>
2023-02-11 14:19:00 +0900

    BaekJoon: No1252

--- baekjoon/src/tags/math/No1252.kt
+++ baekjoon/src/tags/math/No1252.kt
@@ -4,7 +4,6 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.math.BigInteger
 
 /**
  * https://www.acmicpc.net/problem/1252
@@ -13,32 +12,10 @@ private val reader = BufferedReader(InputStreamReader(System.`in`))
 private val writer = BufferedWriter(OutputStreamWriter(System.out))
 
 fun main() {
-    val decimal = reader.readLine().split(" ")
-        .map { binaryToDecimal(it) }
+    val binary = reader.readLine().split(" ")
+        .map { it.toBigInteger(2) }
         .sumOf { it }
-    writer.write(toBinaryString(decimal))
+        .toString(2)
+    writer.write(binary)
     writer.flush()
-}
-
-private fun binaryToDecimal(binary: String): BigInteger {
-    var sum = BigInteger.ZERO
-    binary.reversed().forEachIndexed { index, bit ->
-        if (bit.digitToInt() == 1) {
-            sum = sum.plus(BigInteger.valueOf(2).pow(index))
-        }
-    }
-    return sum
-}
-
-private fun toBinaryString(decimal: BigInteger): String {
-    val binary = StringBuilder()
-    var currentValue = decimal
-    val two = BigInteger.valueOf(2)
-
-    while (currentValue.divide(two) != BigInteger.ZERO) {
-        binary.append(currentValue.mod(two))
-        currentValue = currentValue.divide(two)
-    }
-    binary.append(currentValue)
-    return binary.reverse().toString()
 }
