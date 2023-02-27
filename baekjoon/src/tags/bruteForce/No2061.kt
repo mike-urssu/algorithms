0fdb67f39061e1d0c1722e0bf7ef14f8cab5cd44
mike-urssu <mike.urssu@gmail.com>
2023-02-27 11:13:22 +0900

    BaekJoon: No2061

---
+++ baekjoon/src/tags/bruteForce/No2061.kt
@@ -0,0 +1,17 @@
+package tags.bruteForce
+
+import java.math.BigInteger
+
+/**
+ * https://www.acmicpc.net/problem/2061
+ */
+fun main() {
+    val (p, k) = readln().split(" ").map { it.toBigInteger() }
+    for (i in 2 until k.toInt()) {
+        if (p.mod(i.toBigInteger()) == BigInteger.ZERO) {
+            println("BAD $i")
+            return
+        }
+    }
+    println("GOOD")
+}
