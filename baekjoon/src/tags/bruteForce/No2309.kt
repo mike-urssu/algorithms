7d65a9b6b7200885cb2e1c10f42ba82aa6b5f48a
mike-urssu <mike.urssu@gmail.com>
2023-03-07 17:03:26 +0900

    BaekJoon: No2309

---
+++ baekjoon/src/tags/bruteForce/No2309.kt
@@ -0,0 +1,25 @@
+package tags.bruteForce
+
+/**
+ * https://www.acmicpc.net/problem/2309
+ */
+fun main() {
+    val heights = IntArray(9) { readln().toInt() }
+    val sum = heights.sum()
+
+    for (i in heights.indices) {
+        for (j in heights.indices) {
+            if (i == j) {
+                continue
+            }
+
+            if (sum - heights[i] - heights[j] == 100) {
+                heights[i] = Int.MAX_VALUE
+                heights[j] = Int.MAX_VALUE
+
+                heights.sorted().take(7).forEach { println(it) }
+                return
+            }
+        }
+    }
+}
