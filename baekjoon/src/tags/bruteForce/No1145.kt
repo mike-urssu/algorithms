bce041027a7556c43d6f0cf5c481d12f6836c322
mike-urssu <mike.urssu@gmail.com>
2023-03-07 10:50:11 +0900

    BaekJoon: No1145

---
+++ baekjoon/src/tags/bruteForce/No1145.kt
@@ -0,0 +1,18 @@
+package tags.bruteForce
+
+/**
+ * https://www.acmicpc.net/problem/1145
+ */
+fun main() {
+    val numbers = readln().split(" ").map { it.toInt() }
+
+    var i = 1
+    while (true) {
+        if (numbers.count { i % it == 0 } >= 3) {
+            break
+        }
+        i++
+    }
+
+    println(i)
+}
