3446406b3424e446fc4d46bf2f04428ba9e21437
mike-urssu <mike.urssu@gmail.com>
2023-03-08 09:59:43 +0900

    BaekJoon: No2231

---
+++ baekjoon/src/tags/bruteForce/No2231.kt
@@ -0,0 +1,18 @@
+package tags.bruteForce
+
+/**
+ * https://www.acmicpc.net/problem/2231
+ */
+fun main() {
+    val n = readln().toInt()
+    for (i in 1..1000000) {
+        if (n == getDecomposition(i)) {
+            println(i)
+            return
+        }
+    }
+    println(0)
+}
+
+private fun getDecomposition(number: Int) =
+    number + number.toString().sumOf { it.digitToInt() }
