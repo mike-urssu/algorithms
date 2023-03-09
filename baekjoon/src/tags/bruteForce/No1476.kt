7941f00359acd6fba86196b5f100d1efb9df8430
mike-urssu <mike.urssu@gmail.com>
2023-03-09 12:06:32 +0900

    BaekJoon: No1476

---
+++ baekjoon/src/tags/bruteForce/No1476.kt
@@ -0,0 +1,25 @@
+package tags.bruteForce
+
+/**
+ * https://www.acmicpc.net/problem/1476
+ */
+fun main() {
+    val (e1, s1, m1) = readln().split(" ").map { it.toInt() }
+    var e2 = 1
+    var s2 = 1
+    var m2 = 1
+
+    var count = 1
+    while (true) {
+        if (e1 == e2 && s1 == s2 && m1 == m2) {
+            break
+        }
+
+        e2 = ((e2 + 1) % 16).coerceAtLeast(1)
+        s2 = ((s2 + 1) % 29).coerceAtLeast(1)
+        m2 = ((m2 + 1) % 20).coerceAtLeast(1)
+        count++
+    }
+
+    println(count)
+}
