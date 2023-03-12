a88e5ae9f4c3b7c1bada31f798707228a58bf5ec
mike-urssu <mike.urssu@gmail.com>
2023-03-12 15:08:33 +0900

    BaekJoon: No1057

---
+++ baekjoon/src/tags/bruteForce/No1057.kt
@@ -0,0 +1,15 @@
+package tags.bruteForce
+
+/**
+ * https://www.acmicpc.net/problem/1057
+ */
+fun main() {
+    var (_, a, b) = readln().split(" ").map { it.toInt() }
+    var round = 0
+    while (a != b) {
+        a = (a + 1) / 2
+        b = (b + 1) / 2
+        round++
+    }
+    println(round)
+}
