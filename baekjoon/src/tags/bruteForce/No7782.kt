ea40f58cc6809581a8678ca24906fc9911b18216
mike-urssu <mike.urssu@gmail.com>
2023-03-14 11:32:06 +0900

    BaekJoon: No7782

---
+++ baekjoon/src/tags/bruteForce/No7782.kt
@@ -0,0 +1,23 @@
+package tags.bruteForce
+
+/**
+ * https://www.acmicpc.net/problem/7782
+ */
+fun main() {
+    val n = readln().toInt()
+    val (b1, b2) = readln().split(" ").map { it.toInt() }
+
+    var isLanded = false
+    repeat(n) {
+        val (lx, ly, hx, hy) = readln().split(" ").map { it.toInt() }
+        if (b1 in lx..hx && b2 in ly..hy) {
+            isLanded = true
+        }
+    }
+
+    if (isLanded) {
+        println("Yes")
+    } else {
+        println("No")
+    }
+}
