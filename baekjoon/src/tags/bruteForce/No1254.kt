e1b225da3041642c30e8c4b41f9cfd864d8356e7
mike-urssu <mike.urssu@gmail.com>
2023-03-17 11:10:31 +0900

    BaekJoon: No1254

---
+++ baekjoon/src/tags/bruteForce/No1254.kt
@@ -0,0 +1,25 @@
+package tags.bruteForce
+
+/**
+ * https://www.acmicpc.net/problem/1254
+ */
+fun main() {
+    val s = readln()
+    for (i in s.indices) {
+        val string = s.substring(i)
+        if (isPalindrome(string)) {
+            println(string.length + i * 2)
+            return
+        }
+    }
+    println(s.length * 2 - 1)
+}
+
+private fun isPalindrome(string: String): Boolean {
+    for (i in 0 until string.length / 2) {
+        if (string[i] != string[string.lastIndex - i]) {
+            return false
+        }
+    }
+    return true
+}
