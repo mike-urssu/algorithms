c173677e17580c18ef05f88d81695e0d76ae5929
mike-urssu <mike.urssu@gmail.com>
2023-03-18 21:31:08 +0900

    BaekJoon: No9094

---
+++ baekjoon/src/tags/bruteForce/No9094.kt
@@ -0,0 +1,21 @@
+package tags.bruteForce
+
+/**
+ * https://www.acmicpc.net/problem/9094
+ */
+fun main() {
+    val testCase = readln().toInt()
+    repeat(testCase) {
+        val (n, m) = readln().split(" ").map { it.toInt() }
+        var count = 0
+        for (a in 1 until n - 1) {
+            for (b in a + 1 until n) {
+                val r = (a * a + b * b + m).toDouble() / (a * b)
+                if (r == r.toInt().toDouble()) {
+                    count++
+                }
+            }
+        }
+        println(count)
+    }
+}
