6767286622d63ff7dcbd110824277d610b76f897
mike-urssu <mike.urssu@gmail.com>
2023-03-20 11:14:32 +0900

    BaekJoon: No4673

---
+++ baekjoon/src/tags/bruteForce/No4673.kt
@@ -0,0 +1,17 @@
+package tags.bruteForce
+
+/**
+ * https://www.acmicpc.net/problem/4673
+ */
+fun main() {
+    val selfNumbers = sortedSetOf<Int>()
+    for (i in 1..9973) {
+        selfNumbers.add(getD(i))
+    }
+
+    (1..10000)
+        .filter { !selfNumbers.contains(it) }
+        .forEach { println(it) }
+}
+
+private fun getD(number: Int) = number + number.toString().toCharArray().sumOf { it.digitToInt() }
