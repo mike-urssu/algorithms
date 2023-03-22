fbddddfeda01b42d25726c177d4de06b31049f02
mike-urssu <mike.urssu@gmail.com>
2023-03-22 09:35:02 +0900

    BaekJoon: No17614

---
+++ baekjoon/src/tags/bruteForce/No17614.kt
@@ -0,0 +1,11 @@
+package tags.bruteForce
+
+/**
+ * https://www.acmicpc.net/problem/17614
+ */
+fun main() {
+    val n = readln().toInt()
+    val numbers = listOf('3', '6', '9')
+    val count = (1..n).sumOf { number -> number.toString().toCharArray().count { it in numbers } }
+    println(count)
+}
