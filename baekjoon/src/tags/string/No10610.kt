0dd96a893b553cc9bf4cac3b7c36d0ffbdd0a53e
mike-urssu <mike.urssu@gmail.com>
2023-03-27 15:03:57 +0900

    BaekJoon: No10610

--- baekjoon/src/tags/string/No10610.kt
+++ baekjoon/src/tags/string/No10610.kt
@@ -4,10 +4,10 @@ package tags.string
  * https://www.acmicpc.net/problem/10610
  */
 fun main() {
-    val n = readln()
-    if (!n.contains("0") || n.sumOf { it.digitToInt() } % 3 != 0) {
+    val n = readln().toCharArray().sortedDescending().joinToString("")
+    if (!n.endsWith("0") || n.sumOf { it.digitToInt() } % 3 != 0) {
         println(-1)
     } else {
-        println(n.toCharArray().sortedDescending().joinToString(""))
+        println(n)
     }
 }
