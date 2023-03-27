ca86acd20114997e8d322b289df76aa67af10822
mike-urssu <mike.urssu@gmail.com>
2023-03-27 09:36:21 +0900

    BaekJoon: No11718

---
+++ baekjoon/src/tags/string/No11718.kt
@@ -0,0 +1,11 @@
+package tags.string
+
+/**
+ * https://www.acmicpc.net/problem/11718
+ */
+fun main() {
+    while (true) {
+        val line = readlnOrNull() ?: break
+        println(line)
+    }
+}
