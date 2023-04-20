c68b8ce220201d36d4185e971952b651ad0cc724
mike-urssu <mike.urssu@gmail.com>
2023-04-20 10:56:03 +0900

    BaekJoon: No14465

--- baekjoon/src/tags/prefixSum/No14465.kt
+++ baekjoon/src/tags/prefixSum/No14465.kt
@@ -5,14 +5,14 @@ package tags.prefixSum
  */
 fun main() {
     val (n, k, b) = readln().split(" ").map { it.toInt() }
-    val trafficLights = IntArray(n + 1)
+    val trafficLights = IntArray(n)
     repeat(b) {
         val index = readln().toInt()
-        trafficLights[index] = 1
+        trafficLights[index - 1] = 1
     }
-    (1..n).forEach { i -> trafficLights[i] += trafficLights[i - 1] }
 
+    val prefixSum = trafficLights.runningFold(0, Int::plus)
     val min = (0..n - k)
-        .minOf { i -> trafficLights[i + k] - trafficLights[i] }
+        .minOf { i -> prefixSum[i + k] - prefixSum[i] }
     println(min)
 }
