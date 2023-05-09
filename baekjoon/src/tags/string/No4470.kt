58b8a83547314f6db513811ee46189a15b0b7afc
mike-urssu <mike.urssu@gmail.com>
2023-05-09 09:39:19 +0900

    BaekJoon: No4470

---
+++ baekjoon/src/tags/string/No4470.kt
@@ -0,0 +1,12 @@
+package tags.string
+
+/**
+ * https://www.acmicpc.net/problem/4470
+ */
+fun main() {
+    val n = readln().toInt()
+    for (i in 1..n) {
+        val s = readln()
+        println("$i. $s")
+    }
+}
