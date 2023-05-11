a217aa6a1efac954ed8d8361615a0710c860d86d
mike-urssu <mike.urssu@gmail.com>
2023-05-11 12:18:08 +0900

    BaekJoon: No5430

--- baekjoon/src/tags/string/No5430.kt
+++ baekjoon/src/tags/string/No5430.kt
@@ -2,6 +2,7 @@ package tags.string
 
 import java.util.Deque
 import java.util.LinkedList
+import java.util.StringTokenizer
 
 /**
  * https://www.acmicpc.net/problem/5430
@@ -11,11 +12,9 @@ fun main() {
     repeat(t) {
         val p = readln()
         readln()
-        val numbers = readln().drop(1).dropLast(1).split(",")
-            .filter { it.isNotEmpty() }
-            .map { it.toInt() }
+        val s = readln()
 
-        val deque: Deque<Int> = LinkedList(numbers)
+        val deque = getDeque(s)
         var isInOrder = true
         var isError = false
 
@@ -36,22 +35,39 @@ fun main() {
             }
         }
 
-        if (isError) {
-            println("error")
-        } else {
-            val answer = StringBuilder()
+        println(getAnswer(deque, isError, isInOrder))
+    }
+}
+
+private fun getDeque(s: String): Deque<Int> {
+    val deque: Deque<Int> = LinkedList()
+    val numbers = StringTokenizer(s, "[],")
+    while (numbers.hasMoreTokens()) {
+        deque.add(numbers.nextToken().toInt())
+    }
+    return deque
+}
+
+private fun getAnswer(deque: Deque<Int>, isError: Boolean, isInOrder: Boolean): String {
+    return if (isError) {
+        "error"
+    } else {
+        val answer = StringBuilder()
+        if (deque.isNotEmpty()) {
             if (isInOrder) {
+                answer.append(deque.pollFirst())
                 while (deque.isNotEmpty()) {
-                    answer.append(deque.pollFirst())
                     answer.append(",")
+                    answer.append(deque.pollFirst())
                 }
             } else {
+                answer.append(deque.pollLast())
                 while (deque.isNotEmpty()) {
-                    answer.append(deque.pollLast())
                     answer.append(",")
+                    answer.append(deque.pollLast())
                 }
             }
-            println("[${answer.removeSuffix(",")}]")
         }
+        "[$answer]"
     }
 }
