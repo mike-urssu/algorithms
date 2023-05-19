f4aad8568d38f89831b2f62fce244d7c83ac4711
mike-urssu <mike.urssu@gmail.com>
2023-05-19 11:36:19 +0900

    BaekJoon: No12904

--- baekjoon/src/tags/string/No12904.kt
+++ baekjoon/src/tags/string/No12904.kt
@@ -1,8 +1,5 @@
 package tags.string
 
-import java.util.Deque
-import java.util.LinkedList
-
 /**
  * https://www.acmicpc.net/problem/12904
  */
@@ -10,31 +7,17 @@ fun main() {
     val s = readln()
     val t = readln()
 
-    val deque: Deque<Char> = LinkedList()
-    t.forEach { deque.add(it) }
-
-    var isInOrder = true
-    while (deque.size != s.length) {
-        if (isInOrder && deque.last == 'A') {
-            deque.pollLast()
-        } else if (isInOrder && deque.last == 'B') {
-            isInOrder = false
-            deque.pollLast()
-        } else if (!isInOrder && deque.first == 'A') {
-            deque.pollFirst()
-        } else if (!isInOrder && deque.first == 'B') {
-            isInOrder = true
-            deque.pollFirst()
+    val str = StringBuilder(t)
+    while (s.length < str.length) {
+        if (str.last() == 'A') {
+            str.deleteCharAt(str.lastIndex)
+        } else {
+            str.deleteCharAt(str.lastIndex)
+            str.reverse()
         }
     }
 
-    val str = if (isInOrder) {
-        deque.joinToString("")
-    } else {
-        deque.joinToString("").reversed()
-    }
-
-    if (s == str) {
+    if (s == str.toString()) {
         println(1)
     } else {
         println(0)
