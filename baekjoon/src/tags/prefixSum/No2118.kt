5f74d2e0c93cffa00f1896b0c3e64b98e085995c
mike-urssu <mike.urssu@gmail.com>
2023-06-01 09:53:01 +0900

    BaekJoon: No2118

---
+++ baekjoon/src/tags/prefixSum/No2118.kt
@@ -0,0 +1,20 @@
+package tags.prefixSum
+
+/**
+ * https://www.acmicpc.net/problem/2118
+ */
+fun main() {
+    val n = readln().toInt()
+    val distances = IntArray(n) { readln().toInt() }
+    val totalDistance = distances.sum()
+
+    val prefixSum = distances.runningFold(0, Int::plus)
+    var max = Int.MIN_VALUE
+    for (i in 1 until n) {
+        for (j in i + 1 until n + 1) {
+            val distance = prefixSum[j] - prefixSum[i]
+            max = max.coerceAtLeast(minOf(distance, totalDistance - distance))
+        }
+    }
+    println(max)
+}
