18310943a13cda8da9bcd76b1f81ede238dca777
mike-urssu <mike.urssu@gmail.com>
2023-06-05 09:37:52 +0900

    BaekJoon: No11816

--- baekjoon/src/tags/string/No11816.kt
+++ baekjoon/src/tags/string/No11816.kt
@@ -6,32 +6,11 @@ package tags.string
 fun main() {
     val x = readln()
     val n = if (x.startsWith("0x")) {
-        hexadecimalToDecimal(x.substring(2))
+        x.substring(2).toInt(16)
     } else if (x.startsWith("0")) {
-        octalToDecimal(x.substring(1))
+        x.substring(1).toInt(8)
     } else {
         x
     }
     println(n)
-}
-
-private fun octalToDecimal(x: String): Int {
-    var n = 0
-    var o = 1
-    for (i in x.lastIndex downTo 0) {
-        n += x[i].digitToInt() * o
-        o *= 8
-    }
-    return n
-}
-
-private fun hexadecimalToDecimal(x: String): Int {
-    val hex = arrayOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f')
-    var n = 0
-    var h = 1
-    for (i in x.lastIndex downTo 0) {
-        n += hex.indexOf(x[i]) * h
-        h *= 16
-    }
-    return n
 }
