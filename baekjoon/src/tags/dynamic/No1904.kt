8da46c18f62ad5440674162d0727afa135aa0bc4
mike-urssu <mike.urssu@gmail.com>
2023-06-21 10:40:11 +0900

    BaekJoon: No1904

---
+++ baekjoon/src/tags/dynamic/No1904.kt
@@ -0,0 +1,13 @@
+package tags.dynamic
+
+/**
+ * https://www.acmicpc.net/problem/1904
+ */
+fun main() {
+    val dp = IntArray(1000001)
+    dp[1] = 1
+    dp[2] = 2
+    (3..1000000).forEach { i -> dp[i] = (dp[i - 2] + dp[i - 1]) % 15746 }
+    val n = readln().toInt()
+    println(dp[n])
+}
