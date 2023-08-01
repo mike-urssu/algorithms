76e180a602ce68146e44047b24ef1882b3af40cc
mike-urssu <mike.urssu@gmail.com>
2023-08-01 18:29:19 +0900

    BaekJoon: level 패키지 삭제

--- baekjoon/src/level/bronze5/No15962.kt
+++
@@ -1,6 +0,0 @@
-package level.bronze5
-
-/**
- * https://www.acmicpc.net/problem/15962
- */
-fun main() = println("파이팅!!")
--- baekjoon/src/level/bronze5/No2557.kt
+++
@@ -1,8 +0,0 @@
-package level.bronze5
-
-/**
- * https://www.acmicpc.net/problem/2557
- */
-fun main() {
-    println("Hello World!")
-}
