bb934159e3b5039121d797244859c28271168430
mike-urssu <mike.urssu@gmail.com>
2023-08-20 15:21:41 +0900

    BaekJoon: No6549

--- baekjoon/src/tags/data_structures/No6549.kt
+++ baekjoon/src/tags/data_structures/No6549.kt
@@ -1,5 +1,7 @@
 package tags.data_structures
 
+import java.util.Stack
+
 /**
  * https://www.acmicpc.net/problem/6549
  */
@@ -10,10 +12,13 @@ fun main() {
         if (n == 0) {
             break
         }
-        println(getMaxArea(rects, 1, n))
+        println(getMaxArea(rects, n))
     }
 }
 
+/**
+ * divide and conquer
+ */
 private fun getMaxArea(rects: List<Long>, low: Int, high: Int): Long {
     if (low == high) {
         return rects[low]
@@ -52,6 +57,39 @@ private fun getMidArea(rects: List<Long>, low: Int, mid: Int, high: Int): Long {
         rightIndex++
         height = height.coerceAtMost(rects[rightIndex])
         maxArea = maxArea.coerceAtLeast(height * (rightIndex - leftIndex + 1))
+    }
+
+    return maxArea
+}
+
+/**
+ * stack
+ */
+private fun getMaxArea(rects: List<Long>, n: Int): Long {
+    var maxArea: Long = 0
+
+    val stack = Stack<Int>()
+    for (i in 1..n) {
+        while (stack.isNotEmpty() && rects[i] <= rects[stack.peek()]) {
+            val height = rects[stack.pop()]
+            val width = if (stack.isEmpty()) {
+                i - 1
+            } else {
+                i - 1 - stack.peek()
+            }
+            maxArea = maxArea.coerceAtLeast(height * width)
+        }
+        stack.push(i)
+    }
+
+    while (stack.isNotEmpty()) {
+        val height = rects[stack.pop()]
+        val width = if (stack.isEmpty()) {
+            n
+        } else {
+            n - stack.peek()
+        }
+        maxArea = maxArea.coerceAtLeast(height * width)
     }
 
     return maxArea
