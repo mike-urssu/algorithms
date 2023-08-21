4380acab69ec461e1bc3cb3a21236291f61b9445
mike-urssu <mike.urssu@gmail.com>
2023-08-21 15:00:21 +0900

    BaekJoon: No17406

---
+++ baekjoon/src/tags/bruteforcing/No17406.kt
@@ -0,0 +1,68 @@
+package tags.bruteforcing
+
+/**
+ * https://www.acmicpc.net/problem/17406
+ */
+private lateinit var graph: Array<IntArray>
+private lateinit var clonedGraph: Array<IntArray>
+private lateinit var rotations: Array<List<Int>>
+
+private lateinit var orders: IntArray
+private lateinit var isVisited: BooleanArray
+
+private var min = Int.MAX_VALUE
+
+fun main() {
+    val (n, _, k) = readln().split(" ").map { it.toInt() }
+    graph = Array(n) { readln().split(" ").map { it.toInt() }.toIntArray() }
+    rotations = Array(k) {
+        val (r, c, s) = readln().split(" ").map { it.toInt() }
+        listOf(r - 1, c - 1, s)
+    }
+
+    orders = IntArray(k)
+    isVisited = BooleanArray(k)
+
+    setMin(k, 0)
+
+    println(min)
+}
+
+private fun setMin(k: Int, index: Int) {
+    if (index == k) {
+        clonedGraph = Array(graph.size) { i -> graph[i].copyOf() }
+        orders.forEach { i ->
+            val (r, c, s) = rotations[i]
+            rotate(clonedGraph, r, c, s)
+        }
+        min = min.coerceAtMost(clonedGraph.minOf { it.sum() })
+        return
+    }
+
+    for (i in 0 until k) {
+        if (!isVisited[i]) {
+            isVisited[i] = true
+            orders[index] = i
+            setMin(k, index + 1)
+            isVisited[i] = false
+        }
+    }
+}
+
+private fun rotate(graph: Array<IntArray>, r: Int, c: Int, s: Int) {
+    val clone = Array(graph.size) { i -> graph[i].copyOf() }
+    for (k in 1..s) {
+        for (j in c + k downTo c - k + 1) {
+            graph[r - k][j] = clone[r - k][j - 1]
+        }
+        for (i in r - k until r + k) {
+            graph[i][c - k] = clone[i + 1][c - k]
+        }
+        for (j in c - k until c + k) {
+            graph[r + k][j] = clone[r + k][j + 1]
+        }
+        for (i in r + k downTo r - k + 1) {
+            graph[i][c + k] = clone[i - 1][c + k]
+        }
+    }
+}
