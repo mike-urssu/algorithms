de210406a4d38bcb40dd6ded7e8870fbcc742bad
mike-urssu <mike.urssu@gmail.com>
2023-09-22 18:20:55 +0900

    BaekJoon: No7795

---
+++ baekjoon/src/tags/binary_search/No7795.kt
@@ -0,0 +1,36 @@
+package tags.binary_search
+
+/**
+ * https://www.acmicpc.net/problem/7795
+ */
+fun main() {
+    val t = readln().toInt()
+    repeat(t) {
+        readln()
+        val a = readln().split(" ").map { it.toInt() }.toIntArray()
+        val b = readln().split(" ").map { it.toInt() }.toIntArray().sortedArray()
+        val count = a.sumOf { getLowerBound(b, it) }
+        println(count)
+    }
+}
+
+private fun getLowerBound(numbers: IntArray, n: Int): Int {
+    var low = 0
+    var high = numbers.size
+    while (low + 1 < high) {
+        val mid = (low + high) / 2
+        if (numbers[mid] > n) {
+            high = mid
+        } else if (numbers[mid] == n) {
+            high = mid
+        } else {
+            low = mid
+        }
+    }
+
+    return if (numbers[low] >= n) {
+        low
+    } else {
+        low + 1
+    }
+}
