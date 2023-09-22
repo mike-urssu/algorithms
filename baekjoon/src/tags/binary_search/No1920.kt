9edfee6bf2c780500b29b70211565ee4077a8a52
mike-urssu <mike.urssu@gmail.com>
2023-09-22 17:44:26 +0900

    BaekJoon: No1920

--- baekjoon/src/tags/binary_search/No1920.kt
+++ baekjoon/src/tags/binary_search/No1920.kt
@@ -1,28 +1,32 @@
 package tags.binary_search
 
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-
 /**
  * https://www.acmicpc.net/problem/1920
  */
-private val reader = BufferedReader(InputStreamReader(System.`in`))
-private val writer = BufferedWriter(OutputStreamWriter(System.out))
-
 fun main() {
-    reader.readLine()
-    val numbers = reader.readLine().split(" ").map { it.toInt() }.sorted()
-    reader.readLine()
-    reader.readLine().split(" ").map { it.toInt() }
-        .forEach {
-            if (numbers.binarySearch(it) >= 0) {
-                writer.write("1")
-            } else {
-                writer.write("0")
-            }
-            writer.newLine()
+    readln()
+    val a = readln().split(" ").map { it.toInt() }.sorted().toIntArray()
+    readln()
+    val numbers = readln().split(" ").map { it.toInt() }
+    numbers.forEach {
+        if (isExist(it, a)) {
+            println(1)
+        } else {
+            println(0)
         }
-    writer.flush()
+    }
+}
+
+private fun isExist(x: Int, a: IntArray): Boolean {
+    var low = 0
+    var high = a.size
+    while (low + 1 < high) {
+        val mid = (low + high) / 2
+        if (a[mid] > x) {
+            high = mid
+        } else {
+            low = mid
+        }
+    }
+    return a[low] == x
 }
