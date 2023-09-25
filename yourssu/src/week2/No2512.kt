f567792b79dde305e6165ce93dbe4365e518784b
mike-urssu <mike.urssu@gmail.com>
2023-09-25 17:54:15 +0900

    BaekJoon: No2512

--- yourssu/src/week2/No2512.kt
+++ yourssu/src/week2/No2512.kt
@@ -1,38 +1,31 @@
 package week2
 
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-
 /**
  * https://www.acmicpc.net/problem/2512
  */
-private val reader = BufferedReader(InputStreamReader(System.`in`))
-private val writer = BufferedWriter(OutputStreamWriter(System.out))
-
 fun main() {
-    reader.readLine()
-    val requests = reader.readLine().split(" ").map { it.toInt() }
-    val budget = reader.readLine().toInt()
-    if (requests.sum() <= budget)
-        writer.write("${requests.sortedDescending()[0]}")
-    else
-        writer.write("${getUpperLimit(requests, budget)}")
-    writer.flush()
+    readln()
+    val budgets = readln().split(" ").map { it.toInt() }.toIntArray()
+    val m = readln().toInt()
+
+    if (budgets.sum() <= m) {
+        println(budgets.max())
+    } else {
+        println(getMaxBudget(budgets, m))
+    }
 }
 
-private fun getUpperLimit(requests: List<Int>, budget: Int): Int {
+private fun getMaxBudget(budgets: IntArray, m: Int): Int {
     var low = 0
-    var high = 1000000000 + 1
-
+    var high = 1000000000
     while (low + 1 < high) {
         val mid = (low + high) / 2
-        if (requests.sumOf { (it.coerceAtMost(mid)) } > budget)
+        val sum = budgets.sumOf { minOf(it, mid) }
+        if (sum > m) {
             high = mid
-        else
+        } else {
             low = mid
+        }
     }
-
     return low
 }
