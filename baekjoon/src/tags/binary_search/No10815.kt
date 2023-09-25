f12022a4d53b531c8f2a8a974079411f83a05d7b
mike-urssu <mike.urssu@gmail.com>
2023-09-25 17:05:02 +0900

    BaekJoon: No10815

--- baekjoon/src/tags/binary_search/No10815.kt
+++ baekjoon/src/tags/binary_search/No10815.kt
@@ -1,45 +1,32 @@
 package tags.binary_search
 
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-
 /**
  * https://www.acmicpc.net/problem/10815
  */
-private val reader = BufferedReader(InputStreamReader(System.`in`))
-private val writer = BufferedWriter(OutputStreamWriter(System.out))
-
 fun main() {
-    reader.readLine()
-    val cards = reader.readLine().split(" ").map { it.toInt() }.sorted()
-
-    reader.readLine()
-    val numbers = reader.readLine().split(" ").map { it.toInt() }
-
-    numbers.forEach { number ->
-        if (contains(cards, number)) {  // cards.binarySearch(number) >= 0
-            writer.write("1 ")
+    readln()
+    val cards = readln().split(" ").map { it.toInt() }.sorted().toIntArray()
+    readln()
+    val numbers = readln().split(" ").map { it.toInt() }
+    numbers.forEach {
+        if (isExist(cards, it)) {
+            print("1 ")
         } else {
-            writer.write("0 ")
+            print("0 ")
         }
     }
-    writer.flush()
 }
 
-private fun contains(numbers: List<Int>, number: Int): Boolean {
+private fun isExist(cards: IntArray, n: Int): Boolean {
     var low = 0
-    var high = numbers.size
-
+    var high = cards.size
     while (low + 1 < high) {
         val mid = (low + high) / 2
-        if (numbers[mid] > number) {
+        if (cards[mid] > n) {
             high = mid
         } else {
             low = mid
         }
     }
-
-    return numbers[low] == number
+    return cards[low] == n
 }
