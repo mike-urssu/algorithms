994ad5bb34f107ecdab58e414ab126d13ca54516
mike-urssu <mike.urssu@gmail.com>
2023-09-26 19:24:22 +0900

    BaekJoon: No2512

--- baekjoon/src/tags/binary_search/No2512.kt
+++ baekjoon/src/tags/binary_search/No2512.kt
@@ -1,36 +1,31 @@
 package tags.binary_search
 
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-
 /**
  * https://www.acmicpc.net/problem/2512
  */
-private val reader = BufferedReader(InputStreamReader(System.`in`))
-private val writer = BufferedWriter(OutputStreamWriter(System.out))
-
 fun main() {
-    reader.readLine()
-    val numbers = reader.readLine().split(" ").map { it.toInt() }
-    val budget = reader.readLine().toInt()
+    readln()
+    val budgets = readln().split(" ").map { it.toInt() }.toIntArray()
+    val m = readln().toInt()
 
-    if (numbers.sum() <= budget) {
-        writer.write("${numbers.max()}")
+    if (budgets.sum() <= m) {
+        println(budgets.max())
     } else {
-        var low = 0
-        var high = budget + 1
-        while (low + 1 < high) {
-            val mid = (low + high) / 2
-            val sum = numbers.sumOf { it.coerceAtMost(mid) }
-            if (sum > budget) {
-                high = mid
-            } else {
-                low = mid
-            }
-        }
-        writer.write("$low")
+        println(getMaxBudget(budgets, m))
     }
-    writer.flush()
+}
+
+private fun getMaxBudget(budgets: IntArray, m: Int): Int {
+    var low = 0
+    var high = 1000000000
+    while (low + 1 < high) {
+        val mid = (low + high) / 2
+        val sum = budgets.sumOf { minOf(it, mid) }
+        if (sum > m) {
+            high = mid
+        } else {
+            low = mid
+        }
+    }
+    return low
 }
