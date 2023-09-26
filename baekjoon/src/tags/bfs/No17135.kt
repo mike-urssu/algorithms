45a1e5a0fb945cd4de28537add31935ae0125e00
mike-urssu <mike.urssu@gmail.com>
2023-09-26 19:16:58 +0900

    BaekJoon: No17135

---
+++ baekjoon/src/tags/bfs/No17135.kt
@@ -0,0 +1,108 @@
+package tags.bfs
+
+/**
+ * https://www.acmicpc.net/problem/17135
+ */
+import kotlin.math.abs
+
+private var n = 0
+private var m = 0
+private var d = 0
+private lateinit var graph: Array<IntArray>
+private val archers = IntArray(3)
+private var enemies = 0
+
+fun main() {
+    val input = readln().split(" ").map { it.toInt() }
+    n = input[0]
+    m = input[1]
+    d = input[2]
+    graph = Array(n) { readln().split(" ").map { it.toInt() }.toIntArray() }
+
+    for (i in 0 until m) {
+        setArcherPositions(0, i)
+    }
+
+    println(enemies)
+}
+
+private fun setArcherPositions(index: Int, value: Int) {
+    archers[index] = value
+
+    if (index == archers.lastIndex) {
+        val clone = Array(n) { i -> graph[i].copyOf() }
+        enemies = enemies.coerceAtLeast(getRemovedEnemies(clone))
+        return
+    }
+
+    for (i in value + 1 until m) {
+        setArcherPositions(index + 1, i)
+    }
+}
+
+
+private fun getRemovedEnemies(graph: Array<IntArray>): Int {
+    var count = 0
+    while (isEnemyExist(graph)) {
+        val enemies = findEnemies(graph)
+        count += enemies.size
+        removeEnemies(graph, enemies)
+        moveEnemies(graph)
+    }
+    return count
+}
+
+private fun isEnemyExist(graph: Array<IntArray>) =
+    graph.any { row -> row.any { it == 1 } }
+
+private fun findEnemies(graph: Array<IntArray>): Set<Pair<Int, Int>> {
+    val enemies = mutableSetOf<Pair<Int, Int>>()
+    for (a in archers) {
+        val enemy = findEnemy(graph, a)
+        if (enemy.isNotEmpty()) {
+            enemies.add(Pair(enemy[0], enemy[1]))
+        }
+    }
+    return enemies
+}
+
+private fun findEnemy(graph: Array<IntArray>, a: Int): List<Int> {
+    var row = -1
+    var col = -1
+    var minDistance = Int.MAX_VALUE
+
+    for (j in 0 until m) {
+        for (i in n - 1 downTo 0) {
+            val distance = getDistance(n, a, i, j)
+            if (graph[i][j] == 1 && d >= distance && minDistance > distance) {
+                row = i
+                col = j
+                minDistance = distance
+            }
+        }
+    }
+
+    return if (row == -1 || col == -1) {
+        emptyList()
+    } else {
+        listOf(row, col)
+    }
+}
+
+private fun getDistance(x1: Int, y1: Int, x2: Int, y2: Int) =
+    abs(x1 - x2) + abs(y1 - y2)
+
+private fun removeEnemies(graph: Array<IntArray>, enemies: Set<Pair<Int, Int>>) {
+    enemies.forEach { (i, j) -> graph[i][j]-- }
+}
+
+private fun moveEnemies(graph: Array<IntArray>) {
+    for (j in 0 until m) {
+        for (i in n - 1 downTo 1) {
+            graph[i][j] = graph[i - 1][j]
+        }
+    }
+    for (j in 0 until m) {
+        graph[0][j] = 0
+    }
+}
