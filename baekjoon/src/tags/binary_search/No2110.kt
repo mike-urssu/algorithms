e2301a94b0799bff9ff775065620e8d4a6bde8db
mike-urssu <mike.urssu@gmail.com>
2023-10-04 17:32:02 +0900

    BaekJoon: No2110

--- baekjoon/src/tags/binary_search/No2110.kt
+++ baekjoon/src/tags/binary_search/No2110.kt
@@ -1,58 +1,94 @@
 package tags.binary_search
 
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-
 /**
  * https://www.acmicpc.net/problem/2110
  */
-private val reader = BufferedReader(InputStreamReader(System.`in`))
-private val writer = BufferedWriter(OutputStreamWriter(System.out))
-
-private lateinit var houses: List<Int>
-private lateinit var numbers: Set<Int>
-
 fun main() {
-    val (n, c) = reader.readLine().split(" ").map { it.toInt() }
-    houses = IntArray(n) { reader.readLine().toInt() }.sorted()
-    numbers = houses.toSortedSet()
-
-    var low = 1
-    var high = houses.last()
-    while (low + 1 < high) {
-        val distance = (low + high) / 2
-        if (getCount(distance) >= c) {
-            low = distance
-        } else {
-            high = distance
-        }
-    }
-
-    writer.write("$low")
-    writer.flush()
+    val (n, c) = readln().split(" ").map { it.toInt() }
+    val houses = IntArray(n) { readln().toInt() }.sorted().toIntArray()
+    println(getMaxDistance(n, c, houses))
 }
 
-private fun getCount(distance: Int): Int {
-    var count = 1
-    var currentNumber = houses.first()
-
-    while (true) {
-        val nextIndex = getNextIndex(currentNumber + distance)
-        if (nextIndex == houses.size) {
-            break
+private fun getMaxDistance(n: Int, c: Int, houses: IntArray): Int {
+    var low = 0
+    var high = 1000000000
+    while (low + 1 < high) {
+        val mid = (low + high) shr 1
+        val count = getCount(houses, n, c, mid)
+        if (c <= count) {
+            low = mid
+        } else {
+            high = mid
         }
-        currentNumber = houses[nextIndex]
-        count++
     }
+    return high
+}
 
+private fun getCount(houses: IntArray, n: Int, c: Int, distance: Int): Int {
+    var count = 1
+    var min = houses[0]
+    for (i in 0 until n - 1) {
+        if (houses[i + 1] - min > distance) {
+            min = houses[i + 1]
+            count++
+        }
+    }
     return count
 }
 
-private fun getNextIndex(number: Int) =
-    if (numbers.contains(number)) {
-        houses.binarySearch(number)
-    } else {
-        -houses.binarySearch(number) - 1
-    }
+//import java.io.BufferedReader
+//import java.io.BufferedWriter
+//import java.io.InputStreamReader
+//import java.io.OutputStreamWriter
+//
+///**
+// * https://www.acmicpc.net/problem/2110
+// */
+//private val reader = BufferedReader(InputStreamReader(System.`in`))
+//private val writer = BufferedWriter(OutputStreamWriter(System.out))
+//
+//private lateinit var houses: List<Int>
+//private lateinit var numbers: Set<Int>
+//
+//fun main() {
+//    val (n, c) = reader.readLine().split(" ").map { it.toInt() }
+//    houses = IntArray(n) { reader.readLine().toInt() }.sorted()
+//    numbers = houses.toSortedSet()
+//
+//    var low = 1
+//    var high = houses.last()
+//    while (low + 1 < high) {
+//        val distance = (low + high) / 2
+//        if (getCount(distance) >= c) {
+//            low = distance
+//        } else {
+//            high = distance
+//        }
+//    }
+//
+//    writer.write("$low")
+//    writer.flush()
+//}
+//
+//private fun getCount(distance: Int): Int {
+//    var count = 1
+//    var currentNumber = houses.first()
+//
+//    while (true) {
+//        val nextIndex = getNextIndex(currentNumber + distance)
+//        if (nextIndex == houses.size) {
+//            break
+//        }
+//        currentNumber = houses[nextIndex]
+//        count++
+//    }
+//
+//    return count
+//}
+//
+//private fun getNextIndex(number: Int) =
+//    if (numbers.contains(number)) {
+//        houses.binarySearch(number)
+//    } else {
+//        -houses.binarySearch(number) - 1
+//    }
