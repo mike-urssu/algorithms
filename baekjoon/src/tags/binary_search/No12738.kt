acc39ae3c9b8d15b1576723c0e7de61661319b41
mike-urssu <mike.urssu@gmail.com>
2023-10-05 17:18:09 +0900

    BaekJoon: No12738

---
+++ baekjoon/src/tags/binary_search/No12738.kt
@@ -0,0 +1,37 @@
+package tags.binary_search
+
+/**
+ * https://www.acmicpc.net/problem/12738
+ */
+fun main() {
+    val n = readln().toInt()
+    val numbers = readln().split(" ").map { it.toInt() }.toIntArray()
+    println(getLCSLength(n, numbers))
+}
+
+private fun getLCSLength(n: Int, numbers: IntArray): Int {
+    val lcs = mutableListOf(-1000000001)
+    for (i in 0 until n) {
+        val lowerBound = getLowerBound(lcs, numbers[i])
+        if (lcs.size == lowerBound) {
+            lcs.add(numbers[i])
+        } else {
+            lcs[lowerBound] = numbers[i]
+        }
+    }
+    return lcs.size - 1
+}
+
+private fun getLowerBound(lcs: List<Int>, n: Int): Int {
+    var low = -1
+    var high = lcs.size
+    while (low + 1 < high) {
+        val mid = (low + high) shr 1
+        if (lcs[mid] >= n) {
+            high = mid
+        } else {
+            low = mid
+        }
+    }
+    return high
+}
