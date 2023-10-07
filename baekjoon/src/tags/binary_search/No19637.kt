3d33b8a0008e8508904e31bcb3dd7efd863dbcb2
mike-urssu <mike.urssu@gmail.com>
2023-10-07 18:44:35 +0900

    BaekJoon: No19637

--- baekjoon/src/tags/binary_search/No19637.kt
+++ baekjoon/src/tags/binary_search/No19637.kt
@@ -1,44 +1,45 @@
 package tags.binary_search
 
+/**
+ * https://www.acmicpc.net/problem/19637
+ */
 import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 
-/**
- * https://www.acmicpc.net/problem/19637
- */
 private val reader = BufferedReader(InputStreamReader(System.`in`))
 private val writer = BufferedWriter(OutputStreamWriter(System.out))
 
 fun main() {
     val (n, m) = reader.readLine().split(" ").map { it.toInt() }
     val names = mutableListOf<String>()
-    val boundaries = mutableListOf<Int>()
+    val numbers = mutableListOf<Int>()
     repeat(n) {
-        val input = reader.readLine().split(" ")
-        names.add(input[0])
-        boundaries.add(input[1].toInt())
+        val (name, number) = reader.readLine().split(" ")
+        if ((numbers.lastOrNull() ?: -1) != number.toInt()) {
+            names.add(name)
+            numbers.add(number.toInt())
+        }
     }
 
     repeat(m) {
         val number = reader.readLine().toInt()
-        val index = getLowerBound(boundaries, number)
-        writer.write("${names[index]}\n")
+        writer.write("${getName(names, numbers, number)}\n")
     }
     writer.flush()
 }
 
-private fun getLowerBound(numbers: List<Int>, number: Int): Int {
+private fun getName(names: List<String>, numbers: List<Int>, number: Int): String {
     var low = -1
-    var high = numbers.lastIndex
+    var high = names.size
     while (low + 1 < high) {
-        val mid = (low + high) / 2
+        val mid = (low + high) shr 1
         if (numbers[mid] >= number) {
             high = mid
         } else {
             low = mid
         }
     }
-    return high
+    return names[high]
 }
