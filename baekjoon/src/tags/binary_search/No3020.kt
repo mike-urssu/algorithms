9a04c59daecc2524015e376c5d461e98e22d9619
mike-urssu <mike.urssu@gmail.com>
2023-10-11 15:00:32 +0900

    BaekJoon: No3020

--- baekjoon/src/tags/binary_search/No3020.kt
+++ baekjoon/src/tags/binary_search/No3020.kt
@@ -1,63 +1,39 @@
 package tags.binary_search
 
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-
 /**
  * https://www.acmicpc.net/problem/3020
  */
-private val reader = BufferedReader(InputStreamReader(System.`in`))
-private val writer = BufferedWriter(OutputStreamWriter(System.out))
-
 fun main() {
-    val (n, h) = reader.readLine().split(" ").map { it.toInt() }
-    val obstacles1 = mutableListOf<Int>()
-    val obstacles2 = mutableListOf<Int>()
-    repeat(n) {
-        val number = reader.readLine().toInt()
-        if (it % 2 == 0) {
-            obstacles1.add(number)
-        } else {
-            obstacles2.add(h - number)
-        }
-    }
-    obstacles1.sort()
-    obstacles2.sort()
+    val (n, h) = readln().split(" ").map { it.toInt() }
+    val obstacles = IntArray(n) { readln().toInt() }
 
-    val counts = sortedMapOf<Int, Int>()
-    for (i in 1..h) {
-        val o1 = n / 2 - getLowerBound(obstacles1, i)
-        val o2 = getUpperBound(obstacles2, i - 1)
-        counts[o1 + o2] = counts.getOrDefault(o1 + o2, 0) + 1
-    }
-
-    val min = counts.keys.min()
-    writer.write("$min ${counts[min]}")
-    writer.flush()
+    val obstaclesToDestroy = findObstaclesToDestroy(n, h, obstacles)
+    val min = obstaclesToDestroy.min()
+    println("$min ${obstaclesToDestroy.count { it == min }}")
 }
 
-private fun getLowerBound(numbers: List<Int>, number: Int): Int {
+private fun findObstaclesToDestroy(n: Int, h: Int, obstacles: IntArray): IntArray {
+    val evenObstaclesToDestroy = getEvenObstaclesToDestroy(n, h, obstacles)
+    val oddObstaclesToDestroy = getOddObstaclesToDestroy(n, h, obstacles)
+    return IntArray(h) { i -> evenObstaclesToDestroy[i] + oddObstaclesToDestroy[i] }
+}
+
+private fun getEvenObstaclesToDestroy(n: Int, h: Int, obstacles: IntArray): IntArray {
+    val o = obstacles.filterIndexed { i, _ -> i % 2 == 0 }.sorted()
+    return IntArray(h) { i -> n / 2 - getLowerBound(o, i + 1) }
+}
+
+private fun getOddObstaclesToDestroy(n: Int, h: Int, obstacles: IntArray): IntArray {
+    val o = obstacles.filterIndexed { i, _ -> i % 2 != 0 }.sorted()
+    return IntArray(h) { i -> n / 2 - getLowerBound(o, h - i) }
+}
+
+private fun getLowerBound(numbers: List<Int>, n: Int): Int {
     var low = -1
     var high = numbers.size
     while (low + 1 < high) {
-        val mid = (low + high) / 2
-        if (numbers[mid] < number) {
-            low = mid
-        } else {
-            high = mid
-        }
-    }
-    return high
-}
-
-private fun getUpperBound(numbers: List<Int>, number: Int): Int {
-    var low = -1
-    var high = numbers.size
-    while (low + 1 < high) {
-        val mid = (low + high) / 2
-        if (numbers[mid] > number) {
+        val mid = (low + high) shr 1
+        if (numbers[mid] >= n) {
             high = mid
         } else {
             low = mid
