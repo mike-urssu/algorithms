51099eb234281f62cd177a91f0cf05469ff562af
mike-urssu <mike.urssu@gmail.com>
2023-10-12 17:57:54 +0900

    BaekJoon: No13702

--- baekjoon/src/tags/binary_search/No13702.kt
+++ baekjoon/src/tags/binary_search/No13702.kt
@@ -1,32 +1,24 @@
 package tags.binary_search
 
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-
 /**
  * https://www.acmicpc.net/problem/13702
  */
-private val reader = BufferedReader(InputStreamReader(System.`in`))
-private val writer = BufferedWriter(OutputStreamWriter(System.out))
-
 fun main() {
-    val (n, k) = reader.readLine().split(" ").map { it.toInt() }
-    val drinks = List(n) { reader.readLine().toLong() }
+    val (n, k) = readln().split(" ").map { it.toInt() }
+    val capacities = IntArray(n) { readln().toInt() }
+    println(findMax(capacities, k))
+}
 
+private fun findMax(capacities: IntArray, k: Int): Long {
     var low = 0L
     var high = Int.MAX_VALUE.toLong()
     while (low + 1 < high) {
-        val mid = (low + high) / 2
-        val div = drinks.sumOf { it / mid }
-        if (div < k) {
+        val mid = (low + high) shr 1
+        if (capacities.sumOf { it / mid } < k) {
             high = mid
         } else {
             low = mid
         }
     }
-
-    writer.write("$low")
-    writer.flush()
+    return low
 }
