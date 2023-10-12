e7011b013242e10a77d24a6e4cf17d9bbd7b100f
mike-urssu <mike.urssu@gmail.com>
2023-10-12 16:42:12 +0900

    BaekJoon: No13706

--- baekjoon/src/tags/binary_search/No13706.kt
+++ baekjoon/src/tags/binary_search/No13706.kt
@@ -1,32 +1,25 @@
 package tags.binary_search
 
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-import java.math.BigInteger
-
 /**
  * https://www.acmicpc.net/problem/13706
  */
-private val reader = BufferedReader(InputStreamReader(System.`in`))
-private val writer = BufferedWriter(OutputStreamWriter(System.out))
+import java.math.BigInteger
 
 fun main() {
-    val number = reader.readLine().toBigInteger()
+    val n = readln().toBigInteger()
+    println(sqrt(n))
+}
 
+private fun sqrt(n: BigInteger): BigInteger {
     var low = BigInteger.ZERO
     var high = BigInteger("9".repeat(800))
     while (low.plus(BigInteger.ONE) < high) {
         val mid = low.plus(high).divide(BigInteger.valueOf(2))
-        val pow = mid.pow(2)
-        if (pow > number) {
+        if (mid.pow(2) >= n) {
             high = mid
         } else {
             low = mid
         }
     }
-
-    writer.write("$low")
-    writer.flush()
+    return high
 }
