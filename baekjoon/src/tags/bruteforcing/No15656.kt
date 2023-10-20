62a3ab3c5c2715e65456032c9ff48e01d434d8d4
mike-urssu <mike.urssu@gmail.com>
2023-10-20 19:11:54 +0900

    BaekJoon: No15656

---
+++ baekjoon/src/tags/bruteforcing/No15656.kt
@@ -0,0 +1,27 @@
+package tags.bruteforcing
+
+/**
+ * https://www.acmicpc.net/problem/15656
+ */
+private lateinit var numbers: IntArray
+private lateinit var combination: IntArray
+private val sequences = StringBuilder()
+
+fun main() {
+    val (n, m) = readln().split(" ").map { it.toInt() }
+    numbers = readln().split(" ").map { it.toInt() }.sorted().toIntArray()
+    combination = IntArray(m)
+    (0 until n).forEach { combination(n, m, 0, it) }
+    println(sequences)
+}
+
+private fun combination(n: Int, r: Int, cIndex: Int, nIndex: Int) {
+    combination[cIndex] = numbers[nIndex]
+
+    if (cIndex == r - 1) {
+        sequences.append("${combination.joinToString(" ")}\n")
+        return
+    }
+
+    (0 until n).forEach { combination(n, r, cIndex + 1, it) }
+}
