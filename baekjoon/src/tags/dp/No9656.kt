9354e2f2160884e14585eb3dff48e994002590e2
mike-urssu <mike.urssu@gmail.com>
2023-11-03 15:35:13 +0900

    BaekJoon: No9656

---
+++ baekjoon/src/tags/dp/No9656.kt
@@ -0,0 +1,13 @@
+package tags.dp
+
+/**
+ * https://www.acmicpc.net/problem/9656
+ */
+fun main() {
+    val n = readln().toInt()
+    if (n % 2 == 0) {
+        println("SK")
+    } else {
+        println("CY")
+    }
+}
