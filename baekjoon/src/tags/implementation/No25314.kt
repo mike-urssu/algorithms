586fc98f252c1b7526735f58852f14c5dfe01bbc
mike-urssu <mike.urssu@gmail.com>
2023-11-05 19:53:31 +0900

    BaekJoon: No25314

---
+++ baekjoon/src/tags/implementation/No25314.kt
@@ -0,0 +1,9 @@
+package tags.implementation
+
+/**
+ * https://www.acmicpc.net/problem/25314
+ */
+fun main() {
+    val n = readln().toInt()
+    println("${"long ".repeat((n - 4) / 4)}long int")
+}
