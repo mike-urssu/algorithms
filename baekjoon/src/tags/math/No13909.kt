f06601dcbec0d8b407b1dc6d147bd420b709ace2
mike-urssu <mike.urssu@gmail.com>
2023-11-23 19:23:30 +0900

    BaekJoon: No13909

---
+++ baekjoon/src/tags/math/No13909.kt
@@ -0,0 +1,11 @@
+package tags.math
+
+/**
+ * https://www.acmicpc.net/problem/13909
+ */
+import kotlin.math.sqrt
+
+fun main() {
+    val n = readln().toInt()
+    println(sqrt(n.toDouble()).toInt())
+}
