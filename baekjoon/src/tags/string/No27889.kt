d29e7e2b6c87f752200034752225e29693e6a43f
mike-urssu <mike.urssu@gmail.com>
2023-12-07 15:07:05 +0900

    BaekJoon: No27889

---
+++ baekjoon/src/tags/string/No27889.kt
@@ -0,0 +1,13 @@
+package tags.string
+
+/**
+ * https://www.acmicpc.net/problem/27889
+ */
+fun main() {
+    when (readln()) {
+        "NLCS" -> println("North London Collegiate School")
+        "BHA" -> println("Branksome Hall Asia")
+        "KIS" -> println("Korea International School")
+        "SJA" -> println("St. Johnsbury Academy")
+    }
+}
