57d199dfae3d03dbdddc87b942af74c39fe731c8
mike-urssu <mike.urssu@gmail.com>
2023-12-21 14:59:57 +0900

    BaekJoon: No15680

---
+++ baekjoon/src/tags/implementation/No15680.kt
@@ -0,0 +1,13 @@
+package tags.implementation
+
+/**
+ * https://www.acmicpc.net/problem/15680
+ */
+fun main() {
+    val n = readln().toInt()
+    if (n == 0) {
+        println("YONSEI")
+    } else {
+        println("Leading the Way to the Future")
+    }
+}
