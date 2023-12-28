9549445ed1d4e1886104d509d7ee77807b2cbe95
mike-urssu <mike.urssu@gmail.com>
2023-12-28 21:47:46 +0900

    BaekJoon: No9316

---
+++ baekjoon/src/tags/implementation/No9316.kt
@@ -0,0 +1,9 @@
+package tags.implementation
+
+/**
+ * https://www.acmicpc.net/problem/9316
+ */
+fun main() {
+    val n = readln().toInt()
+    (1..n).forEach { println("Hello World, Judge $it!") }
+}
