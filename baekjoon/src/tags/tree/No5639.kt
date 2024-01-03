a09b8d89d4f6a66d4a6bac7035b1e49eef22708b
mike-urssu <mike.urssu@gmail.com>
2024-01-03 17:27:56 +0900

    BaekJoon: No5639

---
+++ baekjoon/src/tags/tree/No5639.kt
@@ -0,0 +1,34 @@
+package tags.tree
+
+/**
+ * https://www.acmicpc.net/problem/5639
+ */
+private lateinit var preOrderTraversal: List<Int>
+
+fun main() {
+    preOrderTraversal = getPreorderTraversal()
+
+    val postOrderTraversal = mutableListOf<Int>()
+    getPostOrderTraversal(postOrderTraversal, 0, preOrderTraversal.lastIndex)
+    postOrderTraversal.forEach { println(it) }
+}
+
+private fun getPreorderTraversal(): List<Int> {
+    val numbers = mutableListOf<Int>()
+    while (true) {
+        val n = (readlnOrNull() ?: break).toInt()
+        numbers.add(n)
+    }
+    return numbers
+}
+
+private fun getPostOrderTraversal(postOrderTraversal: MutableList<Int>, start: Int, end: Int) {
+    if (start > end) {
+        return
+    }
+
+    val mid = (start + 1..end).firstOrNull { i -> preOrderTraversal[start] < preOrderTraversal[i] } ?: (end + 1)
+    getPostOrderTraversal(postOrderTraversal, start + 1, mid - 1)
+    getPostOrderTraversal(postOrderTraversal, mid, end)
+    postOrderTraversal.add(preOrderTraversal[start])
+}
