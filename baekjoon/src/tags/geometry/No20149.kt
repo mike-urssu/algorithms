4bf2c800967b767f2e3a5725d767a5b0121c77d6
mike-urssu <mike.urssu@gmail.com>
2024-01-21 22:53:51 +0900

    BaekJoon: No20149

--- baekjoon/src/tags/geometry/No20149.kt
+++ baekjoon/src/tags/geometry/No20149.kt
@@ -3,73 +3,108 @@ package tags.geometry
 /**
  * https://www.acmicpc.net/problem/20149
  */
+import java.util.Optional
 import kotlin.math.sign
+
+private lateinit var a: Pair<Long, Long>
+private lateinit var b: Pair<Long, Long>
+private lateinit var c: Pair<Long, Long>
+private lateinit var d: Pair<Long, Long>
+
+private var isIntersecting = false
+private var coordinates = Optional.empty<Pair<Double, Double>>()
 
 fun main() {
     val (x1, y1, x2, y2) = readln().split(" ").map { it.toLong() }
     val (x3, y3, x4, y4) = readln().split(" ").map { it.toLong() }
+    setPoints(Pair(x1, y1), Pair(x2, y2), Pair(x3, y3), Pair(x4, y4))
 
-    val ccw1 = ccw(x1, y1, x2, y2, x3, y3)
-    val ccw2 = ccw(x1, y1, x2, y2, x4, y4)
-    val ccw3 = ccw(x3, y3, x4, y4, x1, y1)
-    val ccw4 = ccw(x3, y3, x4, y4, x2, y2)
+    val ccw1 = ccw(a, b, c)
+    val ccw2 = ccw(a, b, d)
+    val ccw3 = ccw(c, d, a)
+    val ccw4 = ccw(c, d, b)
 
-    if (ccw1 * ccw2 == 0 && ccw3 * ccw4 == 0) {
-        val a: Pair<Long, Long>
-        val b: Pair<Long, Long>
-        val c: Pair<Long, Long>
-        val d: Pair<Long, Long>
-
-        if (Pair(x1, y1) isGreaterThan Pair(x2, y2)) {
-            a = Pair(x2, y2)
-            b = Pair(x1, y1)
-        } else {
-            a = Pair(x1, y1)
-            b = Pair(x2, y2)
-        }
-
-        if (Pair(x3, y3) isGreaterThan Pair(x4, y4)) {
-            c = Pair(x4, y4)
-            d = Pair(x3, y3)
-        } else {
-            c = Pair(x3, y3)
-            d = Pair(x4, y4)
-        }
-
+    if (formOneLine(ccw1, ccw2, ccw3, ccw4)) {
         if (d isGreaterThanEqual a && b isGreaterThanEqual c) {
-            println(1)
-            printCoordinatesIfExists(a.first, a.second, b.first, b.second, c.first, c.second, d.first, d.second)
-        } else {
-            println(0)
+            isIntersecting = true
+            if (a == d && a isGreaterThan c) {
+                coordinates = Optional.of(Pair(a.first.toDouble(), a.second.toDouble()))
+            } else if (b == c && c isGreaterThan a) {
+                coordinates = Optional.of(Pair(b.first.toDouble(), b.second.toDouble()))
+            }
         }
     } else {
-        if (ccw1 * ccw2 <= 0 && ccw3 * ccw4 <= 0) {
-            println(1)
-            printCoordinatesIfExists(x1, y1, x2, y2, x3, y3, x4, y4)
-        } else {
-            println(0)
+        if (meetAtOnePoint(ccw1, ccw2, ccw3, ccw4)) {
+            isIntersecting = true
+            coordinates = Optional.of(getCoordinates())
         }
+    }
+
+    if (isIntersecting) {
+        println(1)
+        if (coordinates.isPresent) {
+            val (x, y) = coordinates.get()
+            println("$x $y")
+        }
+    } else {
+        println(0)
     }
 }
 
-private fun ccw(x1: Long, y1: Long, x2: Long, y2: Long, x3: Long, y3: Long) =
-    ((x1 * y2 + x2 * y3 + x3 * y1) - (x2 * y1 + x3 * y2 + x1 * y3)).sign
-
-private fun printCoordinatesIfExists(
-    x1: Long, y1: Long, x2: Long, y2: Long,
-    x3: Long, y3: Long, x4: Long, y4: Long
+private fun setPoints(
+    p1: Pair<Long, Long>, p2: Pair<Long, Long>,
+    p3: Pair<Long, Long>, p4: Pair<Long, Long>
 ) {
-    if ((x2 - x1) * (y4 - y3) - (x4 - x3) * (y2 - y1) == 0L) {
-        if ((x2 == x3 && y2 == y3) && Pair(x3, y3) isGreaterThanEqual Pair(x1, y1)) {
-            println("$x2 $y2")
-        } else if ((x1 == x4 && y1 == y4) && Pair(x1, y1) isGreaterThanEqual Pair(x3, y3)) {
-            println("$x1 $y1")
-        }
+    if (p2 isGreaterThan p1) {
+        a = p1
+        b = p2
     } else {
-        val div = ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)).toDouble()
-        val x = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / div
-        val y = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / div
-        println("$x $y")
+        b = p1
+        a = p2
+    }
+
+    if (p4 isGreaterThan p3) {
+        c = p3
+        d = p4
+    } else {
+        d = p3
+        c = p4
+    }
+}
+
+private fun ccw(p1: Pair<Long, Long>, p2: Pair<Long, Long>, p3: Pair<Long, Long>) =
+    ((p1.first * p2.second + p2.first * p3.second + p3.first * p1.second) -
+            (p2.first * p1.second + p3.first * p2.second + p1.first * p3.second)).sign
+
+private fun formOneLine(
+    ccw1: Int, ccw2: Int,
+    ccw3: Int, ccw4: Int
+) = isParallel() && formOneLineOrMeetAtTheEnd(ccw1, ccw2, ccw3, ccw4)
+
+private fun isParallel() = (b.first - a.first) * (d.second - c.second) == (d.first - c.first) * (b.second - a.second)
+
+private fun formOneLineOrMeetAtTheEnd(
+    ccw1: Int, ccw2: Int,
+    ccw3: Int, ccw4: Int
+) = ccw1 * ccw2 == 0 && ccw3 * ccw4 == 0
+
+private fun meetAtOnePoint(
+    ccw1: Int, ccw2: Int,
+    ccw3: Int, ccw4: Int
+) = ccw1 * ccw2 <= 0 && ccw3 * ccw4 <= 0
+
+private fun getCoordinates(): Pair<Double, Double> {
+    return if (a == c || a == d) {
+        Pair(a.first.toDouble(), a.second.toDouble())
+    } else if (b == c || b == d) {
+        Pair(b.first.toDouble(), b.second.toDouble())
+    } else {
+        val div = ((a.first - b.first) * (c.second - d.second) - (a.second - b.second) * (c.first - d.first)).toDouble()
+        val x = (a.first * b.second - a.second * b.first) * (c.first - d.first) -
+                (a.first - b.first) * (c.first * d.second - c.second * d.first)
+        val y = (a.first * b.second - a.second * b.first) * (c.second - d.second) -
+                (a.second - b.second) * (c.first * d.second - c.second * d.first)
+        (x / div to y / div)
     }
 }
 
