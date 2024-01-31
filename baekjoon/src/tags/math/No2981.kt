7499aa9def8c75ee5705fc8032d8745230ac9cf7
mike-urssu <mike.urssu@gmail.com>
2024-01-31 20:39:00 +0900

    BaekJoon: No2981

---
+++ baekjoon/src/tags/math/No2981.kt
@@ -0,0 +1,38 @@
+package tags.math
+
+/**
+ * https://www.acmicpc.net/problem/2981
+ */
+import java.util.SortedSet
+import kotlin.math.sqrt
+
+fun main() {
+    val n = readln().toInt()
+    val numbers = IntArray(n) { readln().toInt() }.sortedArrayDescending()
+
+    val diff = IntArray(n - 1) { i -> numbers[i] - numbers[i + 1] }
+    val gcd = diff.fold(0) { n1, n2 -> gcd(n1, n2) }
+
+    val factors = getFactors(gcd)
+    factors.remove(1)
+    println(factors.joinToString(" "))
+}
+
+private fun gcd(a: Int, b: Int): Int {
+    return if (b == 0) {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+private fun getFactors(n: Int): SortedSet<Int> {
+    val factors = sortedSetOf<Int>()
+    (1..sqrt(n.toDouble()).toInt())
+        .filter { i -> n % i == 0 }
+        .forEach { i ->
+            factors.add(i)
+            factors.add(n / i)
+        }
+    return factors
+}
