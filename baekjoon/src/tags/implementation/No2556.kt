490c68cc930509a8b4ceeb46c365f820fd50613f
mike-urssu <mike.urssu@gmail.com>
2024-03-23 23:54:27 +0900

    BaekJoon: No2556

---
+++ baekjoon/src/tags/implementation/No2556.kt
@@ -0,0 +1,11 @@
+package tags.implementation
+
+/**
+ * https://www.acmicpc.net/problem/2556
+ */
+fun main() {
+    val n = readln().toInt()
+    repeat(n) {
+        println("*".repeat(n))
+    }
+}
