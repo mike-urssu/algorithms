5e25eb4286167d638bd47f3e468b5152042ad976
joonhee.song <mike.urssu@gmail.com>
2024-04-19 23:26:31 +0900

    BaekJoon: No2393

---
+++ baekjoon/src/tags/implementation/No2393.kt
@@ -0,0 +1,22 @@
+package tags.implementation
+
+/**
+ * https://www.acmicpc.net/problem/2393
+ */
+fun main() {
+    println("""
+  ___  ___  ___
+  | |__| |__| |
+  |           |
+   \_________/
+    \_______/
+     |     |
+     |     |
+     |     |
+     |     |
+     |_____|
+  __/       \__
+ /             \
+/_______________\
+    """.trimIndent())
+}
