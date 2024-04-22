7a7855ff745138b843da6e84f361a8a47bf65327
joonhee.song <mike.urssu@gmail.com>
2024-04-22 23:10:53 +0900

    BaekJoon: No22193

---
+++ baekjoon/src/tags/math/No22193.kt
@@ -0,0 +1,11 @@
+package tags.math
+
+/**
+ * https://www.acmicpc.net/problem/22193
+ */
+fun main() {
+    readln()
+    val a = readln().toBigDecimal()
+    val b = readln().toBigDecimal()
+    println(a.multiply(b))
+}
