c9c8112cc101d27522b33114280a7c49aad11864
joonhee.song <mike.urssu@gmail.com>
2024-05-10 06:20:52 +0900

    BaekJoon: No21598

---
+++ baekjoon/src/tags/implementation/No21598.kt
@@ -0,0 +1,11 @@
+package tags.implementation
+
+/**
+ * https://www.acmicpc.net/problem/21598
+ */
+fun main() {
+    val n = readln().toInt()
+    repeat(n) {
+        println("SciComLove")
+    }
+}
