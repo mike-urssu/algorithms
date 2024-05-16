36cd171b3d133af815735c9bc3982db09f6fd6dd
joonhee.song <mike.urssu@gmail.com>
2024-05-16 21:54:17 +0900

    BaekJoon: No24883

---
+++ baekjoon/src/tags/implementation/No24883.kt
@@ -0,0 +1,13 @@
+package tags.implementation
+
+/**
+ * https://www.acmicpc.net/problem/24883
+ */
+fun main() {
+    val c = readln()[0]
+    if (c == 'N' || c == 'n') {
+        println("Naver D2")
+    } else {
+        println("Naver Whale")
+    }
+}
