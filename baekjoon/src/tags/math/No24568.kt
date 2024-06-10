bee633be6f7e246b8af127bf91f24008ddfd9f89
joonhee.song <mike.urssu@gmail.com>
2024-06-10 22:46:29 +0900

    BaekJoon: No24568

---
+++ baekjoon/src/tags/math/No24568.kt
@@ -0,0 +1,10 @@
+package tags.math
+
+/**
+ * https://www.acmicpc.net/problem/24568
+ */
+fun main() {
+    val r = readln().toInt()
+    val s = readln().toInt()
+    println(r * 8 + s * 3 - 28)
+}
