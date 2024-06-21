7e93ec4bd6670793cafa53c15010163d890acb19
joonhee.song <mike.urssu@gmail.com>
2024-06-21 22:43:35 +0900

    BaekJoon: No24309

---
+++ baekjoon/src/tags/math/No24309.kt
@@ -0,0 +1,11 @@
+package tags.math
+
+/**
+ * https://www.acmicpc.net/problem/24309
+ */
+fun main() {
+    val a = readln().toBigInteger()
+    val b = readln().toBigInteger()
+    val c = readln().toBigInteger()
+    println((b - c) / a)
+}
