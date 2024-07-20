9ce0d187f80681fb1e6063f7bc4f96cdbf9011db
joonhee.song <mike.urssu@gmail.com>
2024-07-20 23:50:08 +0900

    BaekJoon: No26489

---
+++ baekjoon/src/tags/implementation/No26489.kt
@@ -0,0 +1,13 @@
+package tags.implementation
+
+/**
+ * https://www.acmicpc.net/problem/26489
+ */
+fun main() {
+    var count = 0
+    while (true) {
+        readlnOrNull() ?: break
+        count++
+    }
+    println(count)
+}
