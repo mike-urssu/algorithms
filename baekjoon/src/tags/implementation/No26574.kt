6e8e51aab82be0c37cf7c2790979461f17312dc1
joonhee.song <mike.urssu@gmail.com>
2024-08-28 22:18:57 +0900

    BaekJoon: No26574

---
+++ baekjoon/src/tags/implementation/No26574.kt
@@ -0,0 +1,12 @@
+package tags.implementation
+
+/**
+ * https://www.acmicpc.net/problem/26574
+ */
+fun main() {
+    val n = readln().toInt()
+    repeat(n) {
+        val v = readln().toInt()
+        println("$v $v")
+    }
+}
