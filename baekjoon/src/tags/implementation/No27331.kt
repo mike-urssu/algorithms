4a49d9c7533aac04c0c5201028f07c0b4758a418
joonhee.song <mike.urssu@gmail.com>
2024-10-15 21:45:34 +0900

    BaekJoon: No27331

---
+++ baekjoon/src/tags/implementation/No27331.kt
@@ -0,0 +1,10 @@
+package tags.implementation
+
+/**
+ * https://www.acmicpc.net/problem/27331
+ */
+fun main() {
+    val n1 = readln().toInt()
+    val n2 = readln().toInt()
+    println("$n1$n2")
+}
