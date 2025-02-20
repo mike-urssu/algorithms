0df876fb5d03e0bcfec70534f44ffc46612cd0ed
joonhee.song <mike.urssu@gmail.com>
2025-02-20 19:13:18 +0900

    BaekJoon: No14264

---
+++ baekjoon/src/tags/geometry/No14264.kt
@@ -0,0 +1,11 @@
+package tags.geometry
+
+/**
+ * https://www.acmicpc.net/problem/14264
+ */
+import kotlin.math.sqrt
+
+fun main() {
+    val l = readln().toDouble()
+    println(l * l / 2 * sqrt(3.0) / 2)
+}
