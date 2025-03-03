f24ed3a1a760f0c15537e8ba84abe29d8dde8b11
joonhee.song <mike.urssu@gmail.com>
2025-03-03 18:47:24 +0900

    BaekJoon: No14728

--- baekjoon/src/tags/knapsack/No14728.kt
+++ baekjoon/src/tags/knapsack/No14728.kt
@@ -5,17 +5,13 @@ package tags.knapsack
  */
 fun main() {
     val (n, t) = readln().split(" ").map { it.toInt() }
-    val problems = Array(n) {
-        val (k, s) = readln().split(" ").map { it.toInt() }
-        k to s
-    }.sortedBy { it.first }
-
     val knapsack = Array(n + 1) { IntArray(10001) }
-    problems.forEachIndexed { i, (k, s) ->
+    repeat(n) { i ->
+        val (k, s) = readln().split(" ").map { it.toInt() }
         (0 until k).forEach { j ->
             knapsack[i + 1][j] = knapsack[i + 1][j].coerceAtLeast(knapsack[i][j])
         }
-        (k..10000).forEach { j ->
+        (k..t).forEach { j ->
             knapsack[i + 1][j] = knapsack[i][j].coerceAtLeast(knapsack[i][j - k] + s)
         }
     }
