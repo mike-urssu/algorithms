38e98a124dc0829998098af6a47c8bfd1aa5006d
joonhee.song <mike.urssu@gmail.com>
2025-05-05 21:01:02 +0900

    BaekJoon: No14623

---
+++ baekjoon/src/tags/math/No14623.kt
@@ -0,0 +1,10 @@
+package tags.math
+
+/**
+ * https://www.acmicpc.net/problem/14623
+ */
+fun main() {
+    val b1 = readln().toBigInteger(2)
+    val b2 = readln().toBigInteger(2)
+    println(b1.multiply(b2).toString(2))
+}
