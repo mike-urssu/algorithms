a33d573f401af5e256ecc335a61c61ea8ed4c049
joonhee.song <mike.urssu@gmail.com>
2025-07-09 19:00:24 +0900

    BaekJoon: No15372

---
+++ baekjoon/src/tags/math/No15372.kt
@@ -0,0 +1,12 @@
+package tags.math
+
+/**
+ * https://www.acmicpc.net/problem/15372
+ */
+fun main() {
+    val t = readln().toInt()
+    repeat(t) {
+        val n = readln().toLong()
+        println(n * n)
+    }
+}
