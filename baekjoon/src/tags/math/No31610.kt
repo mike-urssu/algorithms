5aaf829d137363d20a1becffed8d5579cfe72fed
joonhee.song <mike.urssu@gmail.com>
2025-07-16 19:45:24 +0900

    BaekJoon: No31610

---
+++ baekjoon/src/tags/math/No31610.kt
@@ -0,0 +1,11 @@
+package tags.math
+
+/**
+ * https://www.acmicpc.net/problem/31610
+ */
+fun main() {
+    val a = readln().toInt()
+    val b = readln().toInt()
+    val c = readln().toInt()
+    println(a * b + c)
+}
