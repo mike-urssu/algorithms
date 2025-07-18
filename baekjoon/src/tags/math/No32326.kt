d691d8c8d87cbb467a10e19b90ccff9cc19f2f16
joonhee.song <mike.urssu@gmail.com>
2025-07-18 21:50:26 +0900

    BaekJoon: No32326

---
+++ baekjoon/src/tags/math/No32326.kt
@@ -0,0 +1,11 @@
+package tags.math
+
+/**
+ * https://www.acmicpc.net/problem/32326
+ */
+fun main() {
+    val r = readln().toInt()
+    val g = readln().toInt()
+    val b = readln().toInt()
+    println(r * 3 + g * 4 + b * 5)
+}
