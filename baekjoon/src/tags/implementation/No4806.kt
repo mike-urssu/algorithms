076886e94125801a7979e464a836120b1ba3c016
joonhee.song <mike.urssu@gmail.com>
2025-07-24 18:49:51 +0900

    BaekJoon: No4806

---
+++ baekjoon/src/tags/implementation/No4806.kt
@@ -0,0 +1,13 @@
+package tags.implementation
+
+/**
+ * https://www.acmicpc.net/problem/4806
+ */
+fun main() {
+    var count = 0
+    while (true) {
+        readlnOrNull() ?: break
+        count++
+    }
+    println(count)
+}
