0689a83cd6826f9d6e03e9b68377629783f891ab
joonhee.song <mike.urssu@gmail.com>
2026-02-15 23:34:20 +0900

    BaekJoon: No31746

---
+++ baekjoon/src/tags/implementation/No31746.kt
@@ -0,0 +1,13 @@
+package tags.implementation
+
+/**
+ * https://www.acmicpc.net/problem/31746
+ */
+fun main() {
+    val n = readln().toInt()
+    if (n % 2 == 0) {
+        println("SciComLove")
+    } else {
+        println("evoLmoCicS")
+    }
+}
