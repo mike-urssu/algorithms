dd7ff0c66860a41588695c1ad1f41154ab81c91a
joonhee.song <mike.urssu@gmail.com>
2026-03-29 21:52:56 +0900

    BaekJoon: No34721

---
+++ baekjoon/src/tags/implementation/No34721.kt
@@ -0,0 +1,11 @@
+package tags.implementation
+
+/**
+ * https://www.acmicpc.net/problem/34721
+ */
+fun main() {
+    val n = readln().toInt()
+    repeat(n) {
+        println("I love DGU")
+    }
+}
