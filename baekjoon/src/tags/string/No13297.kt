6098ad94584ada3775df00230f9d5ef23e0f311e
joonhee.song <mike.urssu@gmail.com>
2026-03-31 22:07:23 +0900

    BaekJoon: No13297

---
+++ baekjoon/src/tags/string/No13297.kt
@@ -0,0 +1,12 @@
+package tags.string
+
+/**
+ * https://www.acmicpc.net/problem/13297
+ */
+fun main() {
+    val n = readln().toInt()
+    repeat(n) {
+        val s = readln()
+        println(s.length)
+    }
+}
