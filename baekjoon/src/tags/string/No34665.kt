7b4ca55eb8134d9b96eb07098e480bb5e0d77300
joonhee.song <mike.urssu@gmail.com>
2026-04-19 22:01:13 +0900

    BaekJoon: No34665

---
+++ baekjoon/src/tags/string/No34665.kt
@@ -0,0 +1,15 @@
+package tags.string
+
+/**
+ * https://www.acmicpc.net/problem/34665
+ */
+fun main() {
+    val a = readln()
+    val b = readln()
+    val v = if (a == b) {
+        0
+    } else {
+        1550
+    }
+    println(v)
+}
